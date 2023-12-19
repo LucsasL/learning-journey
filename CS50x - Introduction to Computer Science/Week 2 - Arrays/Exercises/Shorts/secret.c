21bf7818506150f6531cefb9d6183632115ea739
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-12-18 21:47:53 -0300

    Create secret.c

---
+++ CS50x - Introduction to Computer Science/Week 2 - Arrays/Exercises/Shorts/secret.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+bool check_phrase(string);
+
+int main(void)
+{
+    string phrase = get_string("What's the secret phrase?: ");
+
+    bool correct = check_phrase(phrase);
+
+    if (correct)
+    {
+        printf("Come on in!\n");
+    }
+}
+
+bool check_phrase(string phrase)
+{
+    string password = "Please";
+    if (strcmp(phrase, password) == 0)
+    {
+        return true;
+    }
+    return false;
+}
