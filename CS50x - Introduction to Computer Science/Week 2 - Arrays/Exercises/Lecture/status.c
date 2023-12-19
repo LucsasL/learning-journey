3a0edb56d210123e9a50ab3937654da3b208e423
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-12-18 21:54:45 -0300

    Create status.c

---
+++ CS50x - Introduction to Computer Science/Week 2 - Arrays/Exercises/Lecture/status.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Missing command-line argument\n");
+        return 1;
+    }
+    else
+    {
+        printf("Hello, %s!\n", argv[1]);
+        return 0;
+    }
+}
