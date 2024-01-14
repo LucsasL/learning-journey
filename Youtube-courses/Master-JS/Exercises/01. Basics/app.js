b68d7326252887b2010fc3507e490e6bb0f8823d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-13 22:18:14 -0300

    Update app.js

--- Youtube-courses/Master-JS/Exercises/01. Basics/app.js
+++ Youtube-courses/Master-JS/Exercises/01. Basics/app.js
@@ -16,3 +16,20 @@ console.table({Name: 'Lucsas', Age: 18, Prof: 'Programmer'});
 // Variables: (reserved keyword) (variable name) (value)
 let name = "Lucsas";
 console.log(name);
+
+// Primitive types
+
+// 1. Number
+let num = 10.01;
+
+// Basic math
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+
+let counter = 0;
+counter++;
+console.log(counter);
