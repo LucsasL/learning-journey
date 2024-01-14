df88bb99de14f945f9fdbe191f56a61b846f9293
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-14 04:26:04 -0300

    Create 04. Comparison Operators.js

---
+++ Youtube-courses/Master-JS/Challenges/04. Comparison Operators.js
@@ -0,0 +1,29 @@
+// 1. Create variable name (firstFavNum) & store your favorite number.
+let firstFavNum = 15;
+
+// 2. Create variable name (secondFavNum) & store second fav number.
+let secondFavNum = 7;
+
+// 3. Check (firstFavNum is greater than secondFavNum)
+console.log(firstFavNum > secondFavNum);
+
+// 4. Check (firstFavNum is less than secondFavNum)
+console.log(firstFavNum < secondFavNum);
+
+// 5. Check (firstFavNum is greater than or equal to secondFavNum)
+console.log(firstFavNum >= secondFavNum);
+
+// 6. Check (firstFavNum is less than or equal to secondFavNum)
+console.log(firstFavNum <= secondFavNum);
+
+// 7. Check (firstFavNum is equal to secondFavNum) using strict equality operator.
+console.log(firstFavNum === secondFavNum);
+
+// 8. Check (firstFavNum is equal to secondFavNum) using loose equality operator.
+console.log(firstFavNum == secondFavNum);
+
+// 9. Check (firstFavNum is not equal to secondFavNum) using strict non-equality operator.
+console.log(firstFavNum !== secondFavNum);
+
+// 10. Check (firstFavNum is not equal to secondFavNum) using loose non-equality operator.
+console.log(firstFavNum != secondFavNum);
