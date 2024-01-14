28cb0328d57581d66551eb9656d05ea3117d656d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-13 23:13:08 -0300

    Create 03. Undefined value.js

---
+++ Youtube-courses/Master-JS/Challenges/03. Undefined value.js
@@ -0,0 +1,13 @@
+// 1. Create variable name (isJsProgrammingLanguage) & store true as a value.
+let isJsProgrammingLanguage = true;
+console.log(isJsProgrammingLanguage);
+
+// 2. Create variable name (isJsHard) & store false as a value.
+let isJsHard = false;
+console.log(isJsHard);
+
+// 3. Create Variable name (favNumb) & store your favorite number inside.
+let favNumb = 15;
+
+// 4. Now add favNumber with the value of undefined.
+console.log(favNumb + undefined);
