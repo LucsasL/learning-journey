f371c5179f147dd9e133bc278c0380ff80edbd76
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-14 18:19:50 -0300

    Strings

--- Youtube-courses/Master-JS/Exercises/01. Basics/app.js
+++ Youtube-courses/Master-JS/Exercises/01. Basics/app.js
@@ -68,7 +68,7 @@ console.log(notDefined);
 */
 
 console.log(10 === 10);
-console.log(10 !== '10')
+console.log(10 !== '10');
 
 // Relational Operators
 // console.log(10 > 10); false
@@ -76,4 +76,35 @@ console.log(10 !== '10')
 // console.log(10 >= 10); true
 // console.log(10 <= 10); true
 
-//
+// Strings
+// 1. Concatenation
+let firstName = "Lucas";
+let secondName = "Lira";
+let fullName = firstName.concat(secondName);
+console.log(fullName);
+
+// 2. Append
+firstName += " \"Lucsas\"";
+console.log(firstName);
+
+// 3. Length
+console.log(firstName.length);
+
+// 4. Cases
+// console.log(firstName.toLocaleLowerCase())
+// console.log(firstName.toLocaleUpperCase());
+
+// 5. Slice
+console.log(firstName.slice(0, 2));
+
+// 6. Split & Join
+console.log(firstName.split(' '));
+
+// 7. Includes
+console.log(firstName.includes('X'));
+
+// 8. Trim
+// console.log(firstName.trim());
+
+// 9. Placeholders
+console.log(`${firstName} ${secondName}`);
