c41f155bec9f125a68bfbd9c8fdefbe2d82d169b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-13 22:51:14 -0300

    Second challenge complete

---
+++ Youtube-courses/Master-JS/Challenges/02. Math operations.js
@@ -0,0 +1,23 @@
+// 1. Craete variable name (firstFavNum) & store your favorite number.
+let firstFavNum = 5;
+
+// 2. Create Variable name (secondFavNum) & store your second fav number.
+let secondFavNum = 7;
+
+// 3. Add (firstFavNum & secondFavNum).
+firstFavNum += secondFavNum;
+
+// 4. Subtract (firstFavNum & secondFavNum).
+firstFavNum -= secondFavNum;
+
+// 5. Multiply(firstFavNum & secondFavNum).
+firstFavNum *= secondFavNum;
+
+// 6. Divided (firstFavNum & secondFavNum).
+firstFavNum /= secondFavNum;9
+
+// 3. Check (firstFavNum Mod secondFavNum).
+console.log(firstFavNum % secondFavNum);
+
+// 8. Check the power of (firstFavNum)
+console.log(firstFavNum ** secondFavNum);
