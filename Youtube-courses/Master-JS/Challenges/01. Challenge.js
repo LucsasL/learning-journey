f7570f0ce31f28207c57efea5d2eb00b1f5f74c9
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-13 22:06:04 -0300

    Create 01. Challenge.js

---
+++ Youtube-courses/Master-JS/Challenges/01. Challenge.js
@@ -0,0 +1,14 @@
+// 1. Create variable name (name) & store your name.
+let name = "Lucas Lira";
+
+// 2. Create variable name (whatDoYouWannabecomeInYourLife) & store "programmer"
+let whatDoYouWannabecomeInYourLife = "Programmer";
+
+// 3. Create variable name (gender) & store your gender.
+let gender = "Male";
+
+// 4. Create variable name (twitterHandle) & and store your twitter handle.
+let twitterHandle = "https://twitter.com/LucsasL";
+
+// 5. Finally log all variables to the console.
+console.log(`${name}, ${whatDoYouWannabecomeInYourLife}, ${gender}, ${twitterHandle}`);
