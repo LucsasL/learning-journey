68d1346df2c18e32ff114e9254fa20c125cc6db4
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-15 03:48:04 -0300

    Update app.js

--- Youtube-courses/Master-JS/Exercises/02. Control Flow/app.js
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/app.js
@@ -44,3 +44,71 @@ if (time > 10) {
     console.log(greetings);
 
 }
+
+
+/*
+    Switch Statements
+
+    switch (expression) {
+        case x:
+            print
+            break;
+        
+        case y:
+            print
+            break;       
+    }
+*/
+
+let x = 0;
+let bulb;
+
+switch (x) {
+    case 0:
+        bulb = "off";
+        break;
+    
+    case 1:
+        bulb = "On";
+        break;
+
+    default:
+        bulb = "No value";
+}
+
+console.log(bulb);
+
+let day = "Monday";
+
+switch (day) {
+  case "Monday":
+    console.log(`Today is ${day}`);
+    break;
+
+  case "Tuesday":
+    console.log(`Today is ${day}`);
+    break;
+
+  case "Wednesday":
+    console.log(`Today is ${day}`);
+    break;
+
+  case "Thursday":
+    console.log(`Today is ${day}`);
+    break;
+
+  case "Friday":
+    console.log(`Today is ${day}`);
+    break;
+
+  case "Saturday":
+    console.log(`Today is ${day}`);
+    break;
+
+  case "Sunday":
+    console.log(`Today is ${day}`);
+    break;
+
+  default:
+    console.log("Don't know what day is today.")
+}
