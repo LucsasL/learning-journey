e87611ad0f7779b48c3eab2ee8ba50589a050a80
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-15 04:02:59 -0300

    Create 07. Switch Statement.js

---
+++ Youtube-courses/Master-JS/Challenges/07. Switch Statement.js
@@ -0,0 +1,24 @@
+// 1. Create variable name (fruit) and store "banana".
+let fruit = "Banana";
+
+// 2. If case is "Banana" print "Banana is good!"
+switch (fruit) {
+    case "Banana":
+        console.log("Banana is good!");
+        break;
+
+  // 3. If case is "Orange" print "I am not a fan of orange."
+    case "Orange":
+        console.log("I am not a fan of oranges.");
+        break;
+
+  // 4. If case is "Apple" print "How do you like apples?"
+    case "Apple":
+        console.log("How do you like apples?");
+        break;
+
+  // 5. default "I have never heard of that fruit."
+    default:
+        console.log("I have never heard of that fruit.");
+
+}
