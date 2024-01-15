1256f81cad8a99cc59af1f7cad7710e752f4c05c
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-15 02:58:19 -0300

    New sections

---
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/app.js
@@ -0,0 +1,46 @@
+/*
+    Conditionals statements
+    
+    if (condition) {
+        ...
+    }
+
+    else if (condition) {
+        ...
+    }
+
+    else {
+        ...
+    }
+*/
+let a = 10;
+let b = 20;
+
+if (a > b)
+{
+    console.log("A is greater than B");
+
+} else if (a < b) {
+    console.log("A is less than B");
+
+} else {
+    console.log("A is equal to B");
+
+}
+
+let time = 10;
+let greetings;
+
+if (time > 10) {
+    greeting = "Good Morning";
+    console.log(greetings);
+
+} else if (time < 20) {
+    greetings = "Good Afternoon";
+    console.log(greetings);
+
+} else {
+    greettings = "Good Evening"
+    console.log(greetings);
+
+}
