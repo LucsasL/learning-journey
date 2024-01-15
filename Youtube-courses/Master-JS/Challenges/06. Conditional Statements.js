17ef8c0ab6430200a7e831ebc8f2f04c33aeb560
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-15 03:28:30 -0300

    Fixing file name

--- Youtube-courses/Master-JS/Challenges/06. Conditional Statements.js
+++ Youtube-courses/Master-JS/Challenges/06. Conditional Statements.js
@@ -1,20 +1,21 @@
 // 1. Create variable name (password) store nothing.
-let password;
+let password = "TestingPassword";
 
 // 2. If password is equal to 8 character long print "Welcome"
-if (password.length() === 8) {
+if (password.length === 8) {
   console.log("Welcome");
 
 // 3. If password is less than 8 character long print "Password is too short"
-} else if (password.length() < 8) {
+} else if (password.length < 8) {
   console.log("Password is too short.");
 
 // 4. If password is greater than 8 character long print "Too long password" and "Password should be 8 characters"""
-} else if (password.length() > 8) {
+} else if (password.length > 8) {
   console.log("Too long password.");
+  console.log("Password should be 8 characters")
 
 // 5. If all fails print "Please provide a password."
 } else {
-    console.log("Please provide a password.")
+    console.log("Please provide a password.");
 
 }
