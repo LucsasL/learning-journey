fac91a35fa1284c078a4963bc7e6a30e373ee5dc
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-16 01:11:23 -0300

    Objects

---
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/Objects.js
@@ -0,0 +1,13 @@
+// Objects -> {label: value}
+const person = {
+	firstName: "Lucsas",
+	lastName: "Lira",
+	age: 18,
+	location: ["Planet", "Earth"],
+	isProgrammer: true
+};
+
+// Accessing Items from our object
+person.location.push("America");
+
+console.log(person);
