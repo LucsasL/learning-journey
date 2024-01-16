54557b7781e361be0c447d02351f89c2b64069dc
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-16 00:10:47 -0300

    Logging

--- Youtube-courses/Master-JS/Exercises/01. Basics/app.js
+++ Youtube-courses/Master-JS/Exercises/01. Basics/app.js
@@ -1,18 +1,3 @@
-// Saying hello to the world
-console.log("Hello, world!");
-
-// Cleaning the console
-console.clear();
-
-/*
-    Name: 'Lucsas'
-    Age:   18
-    Prof: 'Programmer'
-*/
-console.table({Name: 'Lucsas', Age: 18, Prof: 'Programmer'});
-
-// log → console.log
-
 // Variables: (reserved keyword) (variable name) (value)
 let name = "Lucsas";
 console.log(name);
---
+++ Youtube-courses/Master-JS/Exercises/01. Basics/logging.js
@@ -0,0 +1,14 @@
+// Saying hello to the world
+console.log("Hello, world!");
+
+// Cleaning the console
+console.clear();
+
+/*
+    Name: 'Lucsas'
+    Age:   18
+    Prof: 'Programmer'
+*/
+console.table({ Name: "Lucsas", Age: 18, Prof: "Programmer" });
+
+// log → console.log
