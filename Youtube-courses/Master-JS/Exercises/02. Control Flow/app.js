66065451736aa2284dfca4c2cc6675e1b7ecce24
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-15 23:48:01 -0300

    Update app.js

--- Youtube-courses/Master-JS/Exercises/02. Control Flow/app.js
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/app.js
@@ -187,3 +187,21 @@ if (password.length >= 8 || password.includes("Web")) {
 let isTrue = true;
 
 console.log(!isTrue);
+
+// Arrays -> 0 index based
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers);
+
+// Accessing items from our array
+const stringArr = ["Eat", "Sleep", "Code", "Repeat"];
+
+console.log(stringArr[0]);
+console.log(stringArr[1]);
+console.log(stringArr[2]);
+console.log(stringArr[3]);
+
+// Nested Array || Two Dimensional Array
+const nestArr = ["One", ["Two", "Three"], 1, true, false];
+
+console.log(nestArr[1][1]);
