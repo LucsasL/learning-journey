46b9844874734a0fb50dc28fca34fc6bb15c03ad
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-15 23:40:33 -0300

    Lofical operators

--- Youtube-courses/Master-JS/Exercises/02. Control Flow/app.js
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/app.js
@@ -144,5 +144,46 @@ while (i <= 5) {
 let j = 0;
 
 do {
-    console.log(`Hello, world! ${i}`)
+    console.log(`Hello, world! ${j}`)
+    j++;
+    
 } while (j <= 5);
+
+/*
+    Logical operators
+
+    1. -> Logical AND (&&)
+        -> TRUE if both the operands/boolean values are true, else evaluates to FALSE
+
+    2. -> Logical OR (||)
+        -> TRUE if either of the operands/boolean values is true. evaluates to false if both are FALSE
+
+    3. -> Logical NOT (!)
+        -> TRUE if the operands is FALSE and vice-versa
+*/
+
+const a2 = true;
+const b2 = false;
+const c = 4;
+
+console.log(a && b);
+console.log(c > 2 && c < 2);
+
+console.log(a || b);
+console.log(b || b);
+console.log(c > 2 || c < 2);
+
+console.log(!a);
+
+let password = "Lucsas WebDev";
+
+if (password.length >= 8 || password.includes("Web")) {
+    console.log("Valid password");
+
+} else {
+    console.log("Invalid password");
+}
+
+let isTrue = true;
+
+console.log(!isTrue);
