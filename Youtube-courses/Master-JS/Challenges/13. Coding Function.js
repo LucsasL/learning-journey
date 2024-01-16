bd0eaab925f2251909e3ba7a08a3107343d79386
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-16 01:49:17 -0300

    Create 13. Coding Function.js

---
+++ Youtube-courses/Master-JS/Challenges/13. Coding Function.js
@@ -0,0 +1,13 @@
+// 1. Create function name (myFunction)
+function myFunction(num1, num2) {
+  return num1 * num2;
+
+}
+
+// 2. This function will take 2 parameters
+// 3. Any number we pass as arguments it will multiply that number.
+// 4. As argument pass your favorite numbers.
+const res = myFunction(5, 7);
+
+// 5. Log the value.
+console.log(res);
