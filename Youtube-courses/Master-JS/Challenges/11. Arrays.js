26e8d2f48fbefad16dd22c8c73625f7ea1e7af5b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-15 23:59:26 -0300

    Create 11. Arrays.js

---
+++ Youtube-courses/Master-JS/Challenges/11. Arrays.js
@@ -0,0 +1,14 @@
+// 1. Create array name (favSingers) store 3 favorite singers.
+const favSingers = ["Zézin da manga", "Ayuwoki", "Kelerson"];
+
+// 2. Log the first singer in that array.
+console.log(favSingers[0]);
+
+// 3. Create array name (favNumbers) and store 4 fav Numbers.
+const favNumbers = [5, 7, 9, 3];
+
+// 4. Create array name (mixedArr) store["string", ["otherarray"], 123, true]
+const mixedArr = ["Lucsas", favSingers, favNumbers, true];
+
+// 5. Now access each item in that array by using [] notation.
+console.log(mixedArr[1][0]);
