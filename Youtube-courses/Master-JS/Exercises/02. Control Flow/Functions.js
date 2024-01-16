fd0c64319fee0d0aa190d9e3249bea766336690a
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-16 01:43:57 -0300

    Create Functions.js

---
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/Functions.js
@@ -0,0 +1,40 @@
+// A function is a block of code that performs a specific task.
+
+// Functions makes the code reusable. You can declare it once and use it multiple times.
+
+// Function makes the program easier as each small task is divided into a function.
+
+// Function increases readability.
+
+// DRY - Don't Repeat Yourself
+
+// Function name(parameterIfAny) {...}
+
+// Function Declaration
+function greet() {
+  console.log("Hello from a function!");
+
+}
+
+function sayHello(name = "Lucsas") {
+  console.log(`Hello, ${name}!`);
+
+}
+
+// Call || Run || Execute
+greet();
+sayHello();
+
+// return -> Function
+function add(x, y) {
+  return x + y;
+
+}
+
+const res1 = add(10, 20);
+const res2 = add(20, 30);
+const res3 = add(50, 40);
+
+console.log(res1);
+console.log(res2);
+console.log(res3);
