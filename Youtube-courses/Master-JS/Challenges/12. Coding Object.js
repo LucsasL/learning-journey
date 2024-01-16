6625de087899b8d96d94bb5088dc77af41fa1b02
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-16 01:23:11 -0300

    Create 12. Coding Object.js

---
+++ Youtube-courses/Master-JS/Challenges/12. Coding Object.js
@@ -0,0 +1,19 @@
+// 1. Create object name (car)
+let car = {};
+
+// 2. Add (type, model, color) as properties
+car.type = "Fast";
+car.Model = "Dunno";
+car.Color = "Red";
+
+// 3. Check the type of object
+console.log(car.type);
+
+// 4. Update the type property to "Toyota"
+car.type = "Toyota";
+
+// 5. Add new property wheels
+car.wheels = 4;
+
+// 6. Log car to the console
+console.log(car);
