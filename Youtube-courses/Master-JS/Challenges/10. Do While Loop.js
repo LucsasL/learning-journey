bf5f4a337a5ae452614c15d8e9475cf06d6e7889
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-15 23:23:39 -0300

    Create 10. Do While Loop.js

---
+++ Youtube-courses/Master-JS/Challenges/10. Do While Loop.js
@@ -0,0 +1,9 @@
+// Print "Your name" 400 times to the console using a do-while-loop.
+// Iteration should start from 20.
+let i = 20;
+
+do {
+    console.log(`Lucsas ${i}`);
+    i++;
+
+} while (i <= 420);
