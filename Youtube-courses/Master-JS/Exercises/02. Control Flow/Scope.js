675362ac7feb6d4311b164267f88f5b6496f6e7d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-17 04:12:08 -0300

    Create Scope.js

---
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/Scope.js
@@ -0,0 +1,17 @@
+// Scope in JavaScript refers to the current context of code, which determines the accessibility of variables to JavaScript.
+
+// There are two types of scopes in JavaScript.
+
+// Global Scope variables are those declared outside of a block.
+
+// Local Scope variables are those declared inside of a block.
+
+let textMessage = "Hi"; // Global variable
+console.log(textMessage);
+
+function showMessage() {
+  let textMessage = "Hello"; // Local variable
+  console.log(textMessage);
+}
+
+showMessage();
