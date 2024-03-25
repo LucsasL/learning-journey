fe7c41dc6fa632c3d82e0f49001cf0ac1900e1a5
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-25 14:34:45 -0300

    Back to the course

--- Youtube-courses/Master-JS/Exercises/02. Control Flow/JSON.js
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/JSON.js
@@ -1,7 +1,20 @@
 // JSON: JavaScript Object Notation
-
+const person = {
+  Name: "Lucas",
+  Age: "18",
+  email: "js@gmail.com",
+  isSubscribed: true,
+  hobbies: ["Reading", "Running", "Cooking"],
+  address: {
+    city: "New York",
+    idk: true,
+  }
+};
 
 // JSON.stringify()
-console.log(JSON.stringify{person});
+const jsonString = JSON.stringify(person);
+console.log(jsonString);
 
 // JSON.parse()
+const parsedObject = JSON.parse(jsonString);
+console.log(parsedObject);
--- Youtube-courses/Master-JS/Exercises/02. Control Flow/setInterval.js
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/setInterval.js
@@ -1,4 +1,29 @@
 // setInterval
-setInterval(function () {
-  
-})
+setInterval(
+  () => console.log("This function will be executed every 2 seconds."),
+  2000
+)
+
+// setTimeout
+setTimeout(
+  () => {
+    console.log("This function will be executed after 3 seconds");
+  },
+  3000
+)
+
+// Stop the interval after 10 seconds
+const intervalId = setInterval(
+  function () {
+    console.log("This function is being executed at the interval");
+  },
+  1000)
+
+console.log(intervalId);
+
+// Stop the interval after 10 seconds
+setTimeout(function () {
+  clearInterval(intervalId);
+  console.log("Interval Stopped");
+
+}, 10000)
