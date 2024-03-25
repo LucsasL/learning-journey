ededfe2445009ecd7f754554c914786fe185930e
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-25 16:00:27 -0300

    Create 16. Arrow Functions.js

---
+++ Youtube-courses/Master-JS/Challenges/16. Arrow Functions.js
@@ -0,0 +1,15 @@
+// REFACTOR THIS CODE TO ES6 ARROW FUNCTIONS.
+
+setTimeout(() => {
+  console.log("Hello");
+  setTimeout(() => {
+    console.log("Hey!")
+    setTimeout(() => {
+      console.log("Namaste");
+      setTimeout(() => {
+        console.log("Hi");
+        setTimeout(() => console.log("Bonjour"), 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
