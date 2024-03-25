5d6fd173cac5576e51aa16dad2b51a7cd76db069
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-25 15:03:37 -0300

    Create 15. Template Strings.js

---
+++ Youtube-courses/Master-JS/Challenges/15. Template Strings.js
@@ -0,0 +1,17 @@
+// 1. Store this in a template string ~
+// The quick
+// brow fox
+// jumps over
+// the lazy dog
+const templateString = `The quick
+brow fox
+jumps over
+the lazy dog`;
+
+// 2. Create two variables (firstName, lastName)
+const firstName = `Lucas`;
+const lastName = `Lira`;
+
+// 3. Use those variables in template string.
+console.log(`My name is: ${firstName} ${lastName} and I saw...
+${templateString}`);
