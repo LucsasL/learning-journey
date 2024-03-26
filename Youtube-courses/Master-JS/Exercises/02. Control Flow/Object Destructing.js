df5bd690cdeb63a74c227475f6cd75c44d93d5ec
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-26 10:26:46 -0300

    Update Object Destructing.js

--- Youtube-courses/Master-JS/Exercises/02. Control Flow/Object Destructing.js
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/Object Destructing.js
@@ -3,3 +3,9 @@
 const student = { name: "Lucas", position: "First", rollNo: 46 };
 const { name, position } = student;
 console.log(name);
+
+// Object destructuring and rest operator
+let { a, b, ...rest } = { a: 100, b: 200, c: 300, d: 400, e: 500 };
+console.log(a);
+console.log(b);
+console.log(rest);
