67759ee3f5b8cd20b00cda5ab4dd2d1dcb0b3e83
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-26 16:14:41 -0300

    Create Nested Destructing.js

---
+++ Youtube-courses/Master-JS/Exercises/02. Control Flow/Nested Destructing.js
@@ -0,0 +1,10 @@
+const songs = [
+  { name: "Lucky You", singer: "Joyner", duration: 4.34 },
+  { name: "Just like you", singer: "NF", duration: 3.23 },
+  { name: "Humble singer", singer: "Kendrick Lamar", duration: 2.33 },
+  { name: "Old Town Road", singer: "Lil Nas X", duration: 1.43 },
+  { name: "Cold Shoulder", singer: "Central Cee", duration: 5.23 },
+];
+
+const [, ,{ singer }] = songs;
+console.log(singer);
