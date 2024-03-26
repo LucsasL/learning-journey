f69f2d2c0dbd711720e51621a47026513da6e864
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-26 19:11:40 -0300

    Create 24. Ternary Operator.js

---
+++ Youtube-courses/Master-JS/Challenges/24. Ternary Operator.js
@@ -0,0 +1,5 @@
+// If the person have money they can "buy products"
+// If the person don't have any money "They should bring money"
+const money = 100;
+const haveMoney = money > 0 ? `Buy products` : `Bring some money`;
+console.log(haveMoney);
