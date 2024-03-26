37dde1fdbdc6ff735c260c5a2dba7a4fdd319935
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-26 09:07:57 -0300

    Create 20. Rest Parameter.js

---
+++ Youtube-courses/Master-JS/Challenges/20. Rest Parameter.js
@@ -0,0 +1,7 @@
+// Create a function which will take unlimited amount od parameters & log all of that params to the console.
+
+function gamesList(...games) {
+  console.log(games);
+}
+
+gamesList("Undertale", "God of War", "Spider-Man", "Minecraft", "Deltarune");
