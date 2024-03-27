0d16491fad309b4480c5ab4bd960ef1eb617cdd0
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-27 18:22:41 -0300

    Update 29. Filter Method.js

--- Youtube-courses/Master-JS/Challenges/29. Filter Method.js
+++ Youtube-courses/Master-JS/Challenges/29. Filter Method.js
@@ -3,4 +3,9 @@
 
 const ages = [32, 33, 16, 40];
 
-console.log(ages.filter(age => age >= 18));
+function checkAdults(age) {
+  return age >= 18;
+}
+
+const res = ages.filter(checkAdults);
+console.log(res);
