0ebc2da7c891020b76d9da32fee55183536d54e1
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-27 18:03:16 -0300

    Create 28. Map Helper.js

---
+++ Youtube-courses/Master-JS/Challenges/28. Map Helper.js
@@ -0,0 +1,7 @@
+// 1. Create array with random numbers.
+const numbers = [1, 2, 3, 4, 5];
+
+// 2. Map over each items in that array, & multiply each item by 10
+const results = numbers.map(num => num * 10);
+
+console.log(results);
