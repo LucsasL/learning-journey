e0f473ddaf23735ae3358baa918e0d54bbfaf68f
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-27 18:27:38 -0300

    Create 30.2 Filter Method.js

---
+++ Youtube-courses/Master-JS/Challenges/30.2 Filter Method.js
@@ -0,0 +1,15 @@
+// 1. Iterate Over "words" array
+
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
+
+const res = words.filter(word => word.length > 6);
+
+// 2. print only those words which length is greater then 6
+console.log(res);
