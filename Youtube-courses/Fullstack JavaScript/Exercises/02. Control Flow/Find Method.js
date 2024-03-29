ef2892b6e2581130b1bd97677bf5a5b3b0b14d4e
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-29 19:09:42 -0300

    Update Find Method.js

--- Youtube-courses/Fullstack JavaScript/Exercises/02. Control Flow/Find Method.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/02. Control Flow/Find Method.js
@@ -1,1 +1,2 @@
-// The find() method is another built-in array helper in JavaScript that allows you to find
+// The find() method is another built-in array helper in JavaScript that allows you to find the first element in an array that matches a specific condition. It returns the value of the first element that satisfies the given testing function, or undefined if no element is found.
+
