d6c85ee159967efeea5e52e3dfa0b28dde68140e
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-30 20:45:38 -0300

    Create 31. Find Method.js

---
+++ Youtube-courses/Fullstack JavaScript/Challenges/31. Find Method.js
@@ -0,0 +1,6 @@
+// 1. Iterate over "ages" array
+// 2. Print only those ages which is greater than 18
+const ages = [3, 10, 18, 20];
+
+const res = ages.find((item) => item > 18);
+console.log(res);
