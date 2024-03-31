1d5e985688f8fe2b97e01eeaeb645603e8666ef4
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-31 20:38:49 -0300

    innerText, textContent and innerHTML

---
+++ Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.3. innerText, textContent, innerHTML/Getting the content of an object.js
@@ -0,0 +1,13 @@
+const p = document.querySelector("p");
+const h1 = document.querySelector("h1");
+
+// innerText
+console.log(p.innerText);
+h1.innerText = "Text Changed";
+
+// textContent
+console.log(p.textContent);
+
+// innerHTML
+console.log(p.innerHTML);
+h1.innerHTML = "<em>Lucsas</em>";
