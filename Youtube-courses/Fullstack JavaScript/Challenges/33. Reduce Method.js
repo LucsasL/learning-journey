2f72282a1094630a87197e30a6e013cd57f160e7
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-31 00:06:56 -0300

    Now everything is all right

--- Youtube-courses/Fullstack JavaScript/Challenges/33. Reduce Method.js
+++ Youtube-courses/Fullstack JavaScript/Challenges/33. Reduce Method.js
@@ -1,15 +1,20 @@
 // Write a function called calculateProduct that takes an array of numbers as an argument and returns the product of all numbers in the array.
 
-// Write the calculateProduct function using reduce() method to achieve this task.
-
 const numbers = [1, 2, 3 ,4, 5];
 
-const product = numbers.reduce((p, c) => {
-  console.log(`Previous: ${p}`);
-  console.log(`Current: ${c}`);
-  console.log(`Value: ${p + c}`);
-  console.log("--------------------");
-  return p + c;
-}, 0);
+// Write the calculateProduct function using reduce() method to achieve this task.
+function calculateProduct(arr) {
+  const product = arr.reduce((p, c) => {
+    console.log(`Previous: ${p}`);
+    console.log(`Current: ${c}`);
+    console.log(`Value: ${p * c}`);
+    console.log("--------------------");
+    return p * c;
+  });
+  
+  return product;
+}
+
+const product = calculateProduct(numbers);
 
 console.log(product);
