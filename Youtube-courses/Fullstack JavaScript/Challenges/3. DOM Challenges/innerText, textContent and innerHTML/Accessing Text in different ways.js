7c946ab3bf83327efdeefd4211b43ebe0e2b4520
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-31 21:11:33 -0300

    Text Access Challenge Completed

---
+++ Youtube-courses/Fullstack JavaScript/Challenges/3. DOM Challenges/innerText, textContent and innerHTML/Accessing Text in different ways.js
@@ -0,0 +1,12 @@
+// Variables
+const divParagraphs = document.querySelectorAll("p");
+
+// 1. Checking the innerText of "first" element
+console.log(divParagraphs[0].innerText);
+
+// 2. Check the textContent of "second" element
+console.log(divParagraphs[1].innerText);
+
+// 2. Check the innerHTML of "third" element
+console.log(divParagraphs[2].innerHTML);
+
---
+++ Youtube-courses/Fullstack JavaScript/Challenges/3. DOM Challenges/innerText, textContent and innerHTML/app.js
@@ -0,0 +1,11 @@
+// Variables
+const divParagraphs = document.querySelectorAll("p");
+
+// 1. Checking the innerText of "first" element
+console.log(divParagraphs[0].innerText);
+
+// 2. Check the textContent of "second" element
+console.log(divParagraphs[1].textContent);
+
+// 2. Check the innerHTML of "third" element
+console.log(divParagraphs[2].innerHTML);
