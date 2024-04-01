3f22b6a6859dd2dbe69b36b16d92e71e77d82256
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-01 16:23:01 -0300

    Things I forgot

--- Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.9. 3 ways to add events/DOM Events.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.9. 3 ways to add events/DOM Events.js
@@ -3,6 +3,8 @@ const body = document.querySelector("body");
 const secondBtn = document.querySelector(".second-btn");
 const bestBtn = document.querySelector(".best");
 const name = document.createElement("h5");
+const para = document.querySelector(".para");
+const input = document.querySelectorAll("input");
 
 name.innerText = "Lucsas :D";
 name.style.color = "blue";
@@ -11,11 +13,23 @@ name.style.color = "blue";
 secondBtn.onclick = function () {
   console.log("Lucsas :D");
   body.appendChild(name);
+  input[0].setAttribute("value", "Learn More");
 };
 
 // Great Way
 bestBtn.addEventListener("click", () => {
   alert("Good job! ;D");
+  input[0].setAttribute("value", "Best Way");
 })
 
 // Event Object
+para.addEventListener("click", (event) => {
+  console.log(event);
+})
+
+input[1].addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(event);
+})
+
+input[0].setAttribute("placeholder", "I did it!");
