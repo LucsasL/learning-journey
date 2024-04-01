9df646eaaad293b109fc42e79f964f2f1a9fefa7
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-31 21:27:24 -0300

    Classlists methods

--- Youtube-courses/Fullstack JavaScript/Challenges/3. DOM Challenges/innerText, textContent and innerHTML/38. Accessing Text in different ways.js
+++ Youtube-courses/Fullstack JavaScript/Challenges/3. DOM Challenges/innerText, textContent and innerHTML/38. Accessing Text in different ways.js
@@ -7,6 +7,6 @@ console.log(divParagraphs[0].innerText);
 // 2. Check the textContent of "second" element
 console.log(divParagraphs[1].innerText);
 
-// 2. Check the innerHTML of "third" element
+// 3. Check the innerHTML of "third" element
 console.log(divParagraphs[2].innerHTML);
 
---
+++ Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.4. Classlist/ClassLists.js
@@ -0,0 +1,14 @@
+// Variables
+const h1 = document.querySelector("h1");
+
+// ClassList
+console.log(h1.classList);
+
+// add()
+// h1.classList.add("styles");
+
+// remove()
+// h1.classList.remove("styles");
+
+// toggle()
+h1.classList.toggle("apple");
