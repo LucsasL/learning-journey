0baa46d0298e32927e79bd1be142b2201ce43b94
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-01 18:21:20 -0300

    Keyboard Events Done

---
+++ Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.10 Events In-Depth/3.10.2 Keyboard/Keyboard Events.js
@@ -0,0 +1,25 @@
+// https://w3scholls.com/jsref/obj_keyboardevent.asp
+
+const input = document.querySelector("input");
+
+// Key pressed Event
+// input.addEventListener("keypress", () => {
+//   console.log("Key pressed");
+// });
+
+// // Keyup Event
+// input.addEventListener("keyup", () => {
+//   console.log("Keyup");
+// });
+
+// // Keydown Event
+// input.addEventListener("keydown", () => {
+//   console.log("Keydown");
+// });
+
+// Useful Properties and methods
+input.addEventListener("keypress", e => {
+  console.log(e.code);
+  console.log(e.ctrlKey);
+  console.log(e.key);
+})
