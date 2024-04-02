7960c4b9cff79f4ef44cf8b41f32a6766d3cc3ad
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-02 02:06:12 -0300

    Did it :D

--- Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.11 Projects/3.11.5 Simple-Calculator/Calculator.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.11 Projects/3.11.5 Simple-Calculator/Calculator.js
@@ -1,3 +1,25 @@
 document.querySelector("#btn").addEventListener("click", () => {
-  // Get 
+  // Get elements from the DOM
+  let num1 = document.querySelector(".num1").value;
+  let num2 = document.querySelector(".num2").value;
+  let result = document.querySelector(".result");
+  let operator = document.querySelector("#selectOp").value;
+
+  switch (operator) {
+    case "+":
+      result.innerHTML = `Result: <br> <span class="big">${Number(num1) + Number(num2)}</span>`;
+      break;
+
+    case "-":
+      result.innerHTML = `Result: <br> <span class="big">${Number(num1) - Number(num2)}</span>`;
+      break;
+
+    case "/":
+      result.innerHTML = `Result: <br> <span class="big">${Number(num1) / Number(num2)}</span>`;
+      break;
+
+    case "*":
+      result.innerHTML = `Result: <br> <span class="big">${Number(num1) * Number(num2)}</span>`;
+      break;
+  }
 })
