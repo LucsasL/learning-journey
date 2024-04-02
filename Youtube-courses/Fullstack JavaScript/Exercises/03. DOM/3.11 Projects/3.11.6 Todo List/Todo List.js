012eed94a7c13d6b426d9a831723f144a122c442
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-02 15:00:46 -0300

    It didn't work yet

--- Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.11 Projects/3.11.6 Todo List/Todo List.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.11 Projects/3.11.6 Todo List/Todo List.js
@@ -1,10 +1,42 @@
-const todoCont = document.querySelector(".todos ul");
+const todoCont = document.querySelector(".todos");
 const form = document.querySelector("form");
 const todoInput = document.querySelectorAll("input");
-const todoLi = document.createElement("li");
 
-todoInput[1].addEventListener("submit", e => {
-  e.preventDefault();
+todoCont.appendChild(list);
+
+function getTodo() {
+  // Creating new elements
+  const list = document.createElement("ul");
+  const todoLi = document.createElement("li");
+
+  // Settings values and styles
   todoLi.innerText = todoInput[0].value;
-  todoCont.appendChild(todoLi);
-})
+
+  // Append out elements to the DOM
+  todoCont.appendChild(list);
+  list.appendChild(todoLi);
+
+  let closeEl = document.createElement("li");
+  closeEl.innerHTML = "&times;";
+  closeEl.classList.add("delete");
+
+  // Attach events
+  closeEl.addEventListener("click", (e) => {
+    todoCont.removeChild(todoLi);
+  });
+
+  todoLi.appendChild(closeEl);
+  todoLi.classList.add("todo");
+  return todoLi;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let value = todoInput.value;
+  if (!value.trim()) {
+    return;
+  }
+
+  todoLi.appendChild(getTodo(value));
+  todoInput.value = "";
+});
