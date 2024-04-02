76ef13643f3335937d5a8877f532012244e5b964
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-01 23:54:20 -0300

    Finishing this SIMPLE project

--- Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.11 Projects/3.11.3 Feedback/Feedback.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/03. DOM/3.11 Projects/3.11.3 Feedback/Feedback.js
@@ -8,6 +8,22 @@ let selectRating = "Satisfied";
 
 ratingsContainer.addEventListener("click", (e) => {
   if (e.target.parentNode.classList.contains("rating")) {
-    console.log(e.target.parentNode.classList.contains("rating"));
+    removeActive();
+    e.target.parentNode.classList.toggle("active");
+    selectRating = e.target.nextElementSibling.innerHTML;
   }
 });
+
+sendButt.addEventListener("click", () => {
+  panel.innerHTML = `
+    <p class="heart">💖</p>
+    <strong>Thank You!</strong> <br />
+    <strong> Feedback? ${selectRating} </strong>
+  `;
+});
+
+function removeActive() {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove("active");
+  }
+}
