ed462cda7483aff1ac1599132ea618523f1d30f0
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-03 16:21:46 -0300

    Just a test with regulat JavaScript

--- Youtube-courses/Fullstack JavaScript/Exercises/04. GSAP/2. Tween/Tween.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/04. GSAP/2. Tween/Tween.js
@@ -1,7 +1,13 @@
-import gsap from "gsap";
+// import gsap from "gsap";
 
-gsap.to(".box", {
-  x: 150,
-  duration: 2,
-  repeat: -1
+const box = document.querySelector(".box");
+
+// gsap.to(box, {
+//   x: 150,
+//   duration: 2,
+//   repeat: -1
+// })
+
+document.addEventListener("click", () => {
+    box.classList.toggle("move");
 })
