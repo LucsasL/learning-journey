b50008708c9eeda423932cc153d0b96bc2bb1eef
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-04 19:23:54 -0300

    Importing GSAP and remaking the animation

--- Youtube-courses/Fullstack JavaScript/Exercises/04. GSAP/2. Tween/Tween.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/04. GSAP/2. Tween/Tween.js
@@ -1,13 +1,8 @@
-// import gsap from "gsap";
-
 const box = document.querySelector(".box");
 
-// gsap.to(box, {
-//   x: 150,
-//   duration: 2,
-//   repeat: -1
-// })
-
-document.addEventListener("click", () => {
-    box.classList.toggle("move");
-})
+gsap.to(".box", {
+  x: 250,
+  y: 150,
+  duration: 2,
+  repeat: -1,
+});
