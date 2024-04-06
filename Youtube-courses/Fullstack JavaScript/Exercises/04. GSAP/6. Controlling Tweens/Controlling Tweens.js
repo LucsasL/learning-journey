a20e1795bcca4b8fccf0f3fdf2ef04ce13c8590e
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-06 17:54:49 -0300

    Why is it not working?

--- Youtube-courses/Fullstack JavaScript/Exercises/04. GSAP/6. Controlling Tweens/Controlling Tweens.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/04. GSAP/6. Controlling Tweens/Controlling Tweens.js
@@ -2,10 +2,12 @@ let play = document.querySelector(".play");
 let pause = document.querySelector(".pause");
 let restart = document.querySelector(".restart");
 
-let tween = gsap.to(".box1", {
+gsap.to(".box1", {
   y: -200,
   duration: 5,
   background: "#8000ff",
-})
+});
 
 play.addEventListener("click", () => tween.play());
+pause.addEventListener("click", () => tween.pause());
+restart.addEventListener("click", () => tween.restart());
