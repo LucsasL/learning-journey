0dda2684aa82c83599c00333842f41c7a0597cfb
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-06 17:57:40 -0300

    Somehow not working (maybe outdated)

--- Youtube-courses/Fullstack JavaScript/Exercises/04. GSAP/6. Controlling Tweens/Controlling Tweens.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/04. GSAP/6. Controlling Tweens/Controlling Tweens.js
@@ -2,10 +2,11 @@ let play = document.querySelector(".play");
 let pause = document.querySelector(".pause");
 let restart = document.querySelector(".restart");
 
-gsap.to(".box1", {
+let tween = gsap.to(".box1", {
   y: -200,
   duration: 5,
   background: "#8000ff",
+  boxShadow: "#8000ff",
 });
 
 play.addEventListener("click", () => tween.play());
