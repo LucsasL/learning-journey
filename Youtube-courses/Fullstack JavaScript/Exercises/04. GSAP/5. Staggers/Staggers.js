896090691e73b8be8fa18246777cdf7760b3a154
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-06 17:23:01 -0300

    Staggers

---
+++ Youtube-courses/Fullstack JavaScript/Exercises/04. GSAP/5. Staggers/Staggers.js
@@ -0,0 +1,18 @@
+gsap.set(".box", {
+  borderRadius: 0,
+  y: 0,
+});
+
+gsap.to(".box", {
+  borderRadius: 100,
+  duration: 1,
+  y: -150,
+  ease: "power1",
+  yoyo: true,
+  repeat: -1,
+  stagger: {
+    amount: 4,
+    each: 0.5,
+    from: 0,
+  }
+});
