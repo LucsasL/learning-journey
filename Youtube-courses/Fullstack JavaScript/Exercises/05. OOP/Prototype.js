f467c49bed446fac39279028e11d1de63aff54f1
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-09 09:46:19 -0300

    Prototype Inheritance

---
+++ Youtube-courses/Fullstack JavaScript/Exercises/05. OOP/Prototype.js
@@ -0,0 +1,12 @@
+// Every JavaScript object has a anonymous property called prototype
+
+// Remember functions & arrays are also an object
+
+// That prototype will be the parent of that object, prototype property is an object by itself, which have same special properties & method
+
+// Every object is associated with another object. The object (a) is associated with [[Prototype]] Object
+
+// -> Which means a object will inherit all properties of Object.prototype || [[Prototype]]
+
+const obj = {};
+console.log(obj);
