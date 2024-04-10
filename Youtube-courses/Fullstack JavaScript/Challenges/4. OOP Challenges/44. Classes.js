46ab4cecffd7e9a73374eea8d49a3edf6ae97720
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-10 03:19:36 -0300

    Update 44. Classes.js

--- Youtube-courses/Fullstack JavaScript/Challenges/4. OOP Challenges/44. Classes.js
+++ Youtube-courses/Fullstack JavaScript/Challenges/4. OOP Challenges/44. Classes.js
@@ -1,1 +1,29 @@
-// 1. 
+// 1. Create a class name "Hero", properties (name, level)
+// 2. Create greet method which will just greet
+class Hero {
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
+
+    this.greet = function () {
+      return `Hello, ${this.name}, level: ${this.level}.`;
+    }
+  };
+}
+
+// 3. Create Instance of "hero" class
+const saitama = new Hero("Saitama", "?");
+
+// 4. Create New class name "Mega", inherit properties from "hero" class & provide property of spell
+class Mega extends Hero {
+  constructor(name, level, spell) {
+    super(name, level);
+    this.name = name;
+    this.level = level;
+    this.spell = spell;
+  }
+}
+
+// 5. Create instance of "Mega class"
+const batman = new Mega("Bruce Wayne", 500);
+console.log(batman.greet());
