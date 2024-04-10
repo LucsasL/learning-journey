99b8724b512c4bcfd275f30b9ae7b69de120b17c
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-10 00:05:16 -0300

    Update Prototype.js

--- Youtube-courses/Fullstack JavaScript/Exercises/05. OOP/Prototype.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/05. OOP/Prototype.js
@@ -4,9 +4,50 @@
 
 // That prototype will be the parent of that object, prototype property is an object by itself, which have same special properties & method
 
-// Every object is associated with another object. The object (a) is associated with [[Prototype]] Object
+// Every object is associated with another object. The object (obj) is associated with [[Prototype]] Object
 
-// -> Which means a object will inherit all properties of Object.prototype || [[Prototype]]
+// -> Which means "obj" object will inherit all properties of Object.prototype || [[Prototype]]
+
+// We can check the prototype of something in 3 ways:
+// -> obj.__proto__;
+// -> obj.constructor.prototype;
+// -> Object.getPrototype(obj);
 
 const obj = {};
 console.log(obj);
+console.log(obj.__proto__);
+console.log(obj.constructor.prototype);
+console.log(Object.getPrototypeOf(obj));
+
+const arr = new Array("Hello", "Yello", "Bla");
+console.log(arr);
+
+function Person(firstName, lastName, pl) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.pl = pl;
+}
+
+const perInst = new Person("Lucas", "Lira", "C");
+console.log(perInst.__proto__.__proto__.__proto__);
+
+// -------------------------------------------
+// Changing the built-in Method
+
+Array.prototype.pop = function () {
+  return "POP IT UP!";
+}
+
+const myArr = [1, 2, 3];
+
+console.log(myArr.pop()); // LOL :D
+console.log(myArr);
+
+// -------------------------------------------
+
+String.prototype.hello = function () {
+  console.log(this);
+  console.log(this.toUpperCase());
+}
+
+console.log("Hello and welcome".hello());
