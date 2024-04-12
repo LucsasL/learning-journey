062e1ef32187b94736213cdbed83c417dae9229b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-11 22:03:03 -0300

    Update Inheritance.js

--- Youtube-courses/Fullstack JavaScript/Exercises/05. OOP/Inheritance.js
+++ Youtube-courses/Fullstack JavaScript/Exercises/05. OOP/Inheritance.js
@@ -25,3 +25,39 @@ Dog.prototype.makeSound = function () {
 const genericAnimal = new Animal("Generic Animal");
 console.log(genericAnimal.name);
 console.log(genericAnimal.makeSound());
+
+const dog1 = new Dog("Buddy");
+console.log(dog1);
+console.log(dog1.name);
+console.log(dog1.makeSound());
+
+// ES6 Classes
+
+// Superclass
+
+class Animal2 {
+  constructor(name) {
+    this.name = name;
+  }
+
+  makeSound(sound) {
+    return sound != undefined ? sound : `Unknown Sound`;
+  }
+}
+
+// Subclass inheriting from Animal
+class Dog extends Animal2 {
+  constructor(name) {
+    super(name);
+  }
+
+  makeSound() {
+    return "Woof!";
+  }
+}
+
+const generic = new Animal("Generic Animal 2");
+console.log(generic.makeSound());
+
+const dog2 = new Dog("Buddy 2");
+console.log(dog2.makeSound());
