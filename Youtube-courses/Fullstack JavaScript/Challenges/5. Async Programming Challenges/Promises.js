34a4d9190bed509c497170eb2eb25bb7e63b5cfb
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-13 01:00:36 -0300

    Create Promises.js

---
+++ Youtube-courses/Fullstack JavaScript/Challenges/5. Async Programming Challenges/Promises.js
@@ -0,0 +1,29 @@
+// Refactor the code below
+
+console.log("Start");
+
+function getUserDataFromDB(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Getting User Name...");
+      console.log("Valid Name")
+      resolve(name);
+    }, 2000);
+  });
+}
+
+function getUserHobbies(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Getting User Hobbies...");
+      resolve(["Code", "Play", "Read", "Walk"]);
+    }, 2000);
+  });
+}
+
+getUserDataFromDB("Irineu").then((name) => {
+  console.log(name);
+  getUserHobbies(name).then((hobby) => console.log(hobby));
+});
+
+console.log("End");
