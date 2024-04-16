8a82864f1591d91d44e5f811d7567837d9d4f7c5
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-16 12:36:25 -0300

    Create Return Annotations.ts

---
+++ Youtube-courses/TypeScript Pro/Exercises/Return Annotations.ts
@@ -0,0 +1,12 @@
+// Regular Function
+function square(x: number) : number {
+  return x * x;
+}
+
+const res = square(8);
+console.log(res);
+
+// Arrow Function
+const square2 = (x: number) : number => x * x;
+const res2 = square2(5);
+console.log(res2);
