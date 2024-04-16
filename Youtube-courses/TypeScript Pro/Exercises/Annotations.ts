8b62f7d8da184578049da42dddce51f717af9b59
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-16 06:05:39 -0300

    Create Annotations.ts

---
+++ Youtube-courses/TypeScript Pro/Exercises/Annotations.ts
@@ -0,0 +1,18 @@
+// Strings
+
+// let myVar: type = value
+let myName: string = "Irineu";
+myName = "Other Person";
+console.log(myName);
+
+// ERROR
+// myName = 12;
+
+// Numbers
+let favNumber: number = 5;
+console.log(favNumber);
+
+// Booleans
+let isHard: boolean = false;
+isHard = true;
+console.log(isHard);
