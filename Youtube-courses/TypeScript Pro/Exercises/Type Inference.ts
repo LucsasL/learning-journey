d0f90a202cf0e9912088d51537f2faa4ba1d037b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-16 10:53:52 -0300

    Create Type Inference.ts

---
+++ Youtube-courses/TypeScript Pro/Exercises/Type Inference.ts
@@ -0,0 +1,11 @@
+// Here we're inferring the types!
+let tech = "TypeScript";
+let favNumber = 5;
+let isHard = true;
+
+console.log(tech, favNumber, isHard);
+
+// ERROR
+// tech = false;
+// favNumber = "Hello";
+// isHard = 20;
