ee60c37a31c63231764827ba7b062a2a791200d0
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-16 13:30:17 -0300

    Create Multi Dimensional.ts

---
+++ Youtube-courses/TypeScript Pro/Exercises/Multi Dimensional.ts
@@ -0,0 +1,30 @@
+// A multi-dimensional array is an array that contains other arrays as its elements. Multi-dimensional arrays can be defined using the same notation as one-dimensional arrays, but with nested square brackets.
+
+// Single Dimensional Array
+
+const singleDi: number[] = [1, 2, 3, 4, 5];
+console.log(singleDi);
+
+// Two Dimensional Array
+
+const matrix: number[][] = [
+  [1, 2],
+  [3, 4],
+];
+
+console.log(matrix);
+
+// Three Dimensional Array
+
+const threeDi: number[][][] = [
+  [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+  ],
+  [
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+  ]
+];
+
+console.log(threeDi);
