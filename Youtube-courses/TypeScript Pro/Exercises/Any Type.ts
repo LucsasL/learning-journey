8fc725262f6e30ec86f1a58c7e98692c2c2a5659
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-16 12:13:31 -0300

    Update Any Type.ts

--- Youtube-courses/TypeScript Pro/Exercises/Any Type.ts
+++ Youtube-courses/TypeScript Pro/Exercises/Any Type.ts
@@ -1,1 +1,9 @@
 // TypeScript has a special any type that can be used to represent any type. When a variable is annotated with the any type, TypeScript will allow it to have any type and disable all type checking for that variable and its properties.
+
+// While the any type can be useful in certain situations, it should be used sparingly. Overuse of any can lead to untyped code and make it harder to catch type-related bugs during development. It's generally better to use more specific types whenever possible to get the benefits of TypeScript's type checking.
+
+let color: any = "crimson";
+color = 20;
+color = true;
+color();
+console.log(color);
