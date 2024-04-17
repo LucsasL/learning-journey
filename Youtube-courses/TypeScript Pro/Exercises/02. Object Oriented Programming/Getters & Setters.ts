c1dadb950c719a048e0642a886d3d1e6c6e5c021
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-17 15:23:25 -0300

    Create Getters & Setters.ts

---
+++ Youtube-courses/TypeScript Pro/Exercises/02. Object Oriented Programming/Getters & Setters.ts
@@ -0,0 +1,1 @@
+// Getters and setters are used to access and modify class properties. Getters and setters allow you to define a property in a class that looks like a simple variable from the outside but internally has additional logic for getting and setting the value.
