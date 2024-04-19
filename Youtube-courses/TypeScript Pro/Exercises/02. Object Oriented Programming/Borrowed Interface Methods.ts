7a73e2cfc171416f77e2b19a2131b02f2527a8f9
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-19 19:48:56 -0300

    Update Borrowed Interface Methods.ts

--- Youtube-courses/TypeScript Pro/Exercises/02. Object Oriented Programming/Borrowed Interface Methods.ts
+++ Youtube-courses/TypeScript Pro/Exercises/02. Object Oriented Programming/Borrowed Interface Methods.ts
@@ -9,8 +9,16 @@ interface MovieGenre extends MovieDetails {
 }
 
 const movie1: MovieGenre = {
-  name: "",
-  ratings: 1,
-  genre: "",
-  }
+  name: "Star Wars",
+  genre: "Action",
+  ratings: 8.9,
+  printMovieInfo(
+    name: string,
+    price: number,
+    ratings: number
+  ): string | number {
+    return `Movie name: ${name} Price: ${price} Ratings: ${ratings}`;
+  },
 };
+
+console.log(movie1.printMovieInfo("Star Wars", 30, 8.9));
