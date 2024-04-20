a878cf622d719d595dedb39f5cea0027c7a4347b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-20 19:30:46 -0300

    Update Generics.ts

--- Youtube-courses/TypeScript Pro/Exercises/04. Generics/Generics.ts
+++ Youtube-courses/TypeScript Pro/Exercises/04. Generics/Generics.ts
@@ -20,3 +20,41 @@ function printInfo<T>(x:T): T {
 const str = printInfo<string>("Hello"); // Hello
 const num = printInfo<number>(5); // 5
 const bool = printInfo<boolean>(true); // true
+
+console.log(str);
+console.log(num);
+console.log(bool);
+console.log("------------------------------");
+
+// Practical Example
+function printString2(item: string, defaultValue: string): [string, string] {
+  return [item, defaultValue];
+}
+
+function printNumber2(item: number, defaultValue: number): [number, number] {
+  return [item, defaultValue];
+}
+
+function printBool2(item: boolean, defaultValue: boolean): [boolean, boolean] {
+  return [item, defaultValue];
+}
+
+const resStr = printString2("Just", "Testing");
+console.log(resStr);
+
+const resNum = printNumber2(5, 4);
+console.log(resNum);
+
+const resBool = printBool2(true, false);
+console.log(resStr);
+console.log("------------------------------");
+
+function uniqueDataTypesFunc<Type>(
+  item: Type,
+  defaultValue: Type
+): [Type, Type] {
+  return [item, defaultValue];
+}
+
+const genericsRes = uniqueDataTypesFunc<(string | number)>(5, "Something");
+console.log(genericsRes);
