87062d5e400f66bf783d19385e5d392a26df4fd6
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-21 08:37:56 -0300

    Update Taking keys from objs with Generics.ts

--- Youtube-courses/TypeScript Pro/Exercises/04. Generics/Taking keys from objs with Generics.ts
+++ Youtube-courses/TypeScript Pro/Exercises/04. Generics/Taking keys from objs with Generics.ts
@@ -1,3 +1,27 @@
-function getRandomKeyValuePair(params: any) {
-  
+function getRandomKeyValuePair<Type>(obj: { [key: string]: Type }): {
+  key: string;
+  value: Type;
+} {
+  const keys = Object.keys(obj);
+  const randKey = keys[Math.floor(Math.random() * keys.length)];
+
+  return { key: randKey, value: obj[randKey] };
 }
+
+const stringObject = {
+  a: "Apple",
+  b: "Banana",
+  c: "Cherry",
+};
+
+const res = getRandomKeyValuePair<string>(stringObject);
+console.log(res);
+
+const numberObject = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+const res2 = getRandomKeyValuePair<number>(numberObject);
+console.log(res2);
