238c8c08c5e9749a6f72ae5b8e089e248899027e
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-23 18:25:09 -0300

    Props Children

--- Youtube-courses/Master React JS/myapp/src/App.js
+++ Youtube-courses/Master React JS/myapp/src/App.js
@@ -8,7 +8,16 @@ function App() {
     age={18}
     isMarried={false}
     hobbies={["Coding", "Reading", "Sleeping"]}
-    />
+    >
+      <h1>
+        Hello I'm a children
+      </h1>
+
+      <p>
+        Some random text because the lorem and the emmet isn't working.
+      </p>
+
+    </User>
   );
 }
 
--- Youtube-courses/Master React JS/myapp/src/components/User.jsx
+++ Youtube-courses/Master React JS/myapp/src/components/User.jsx
@@ -1,11 +1,12 @@
-const User = ({img, name, age, isMarried, hobbies}) => {
+const User = ({img, name, age, isMarried, hobbies, children}) => {
   return (
     <section>
       <img src={img} alt={name} width={200} />
       <h1>Name: {name}</h1>
       <h2>Age: {age}</h2>
-      <h3>Is married: {isMarried}</h3>
+      <h3>Is married: {isMarried ? "Yes" : "No"}</h3>
       <h4>Hobbies: {hobbies}</h4>
+      {children}
     </section>
   );
 };
