6e8531a0b4f677836cb60df6f6bfec2e9009d46b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-23 14:32:50 -0300

    Props Destructuring

--- Youtube-courses/Master React JS/myapp/src/components/User.jsx
+++ Youtube-courses/Master React JS/myapp/src/components/User.jsx
@@ -1,11 +1,11 @@
-const User = (props) => {
+const User = ({img, name, age, isMarried, hobbies}) => {
   return (
     <section>
-      <img src={props.img} alt={props.name} width={200} />
-      <h1>Name: {props.name}</h1>
-      <h2>Age: {props.age}</h2>
-      <h3>Is married: {props.isMarried}</h3>
-      <h4>Hobbies: {props.hobbies}</h4>
+      <img src={img} alt={name} width={200} />
+      <h1>Name: {name}</h1>
+      <h2>Age: {age}</h2>
+      <h3>Is married: {isMarried}</h3>
+      <h4>Hobbies: {hobbies}</h4>
     </section>
   );
 };
