b051ae52219ddabca91c264f8bef105fcca38f96
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-24 18:32:01 -0300

    Events first example

--- Youtube-courses/Master React JS/Exercises/React Events.jsx
+++ Youtube-courses/Master React JS/Exercises/React Events.jsx
@@ -1,7 +1,33 @@
-const Events = () => {
+const Button = () => {
+  const buttonStyle = {
+    background: "blue",
+    width: "200px",
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: "20px",
+    outline: "none",
+    boxShadow: "0px 0px 10px black",
+    cursor: "pointer",
+  };
+
+  const handleClick = (a, b) => {
+    console.log(a + b);
+    alert(`${a} + ${b} = ${a + b}`);
+  };
+
   return (
-    <div>React Events</div>
-  )
+    <button style={buttonStyle} onClick={() => handleClick(2, 2)}>
+      Click
+    </button>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <Button />
+    </>
+  );
 }
 
-export default React Events;
+export default App;
--- Youtube-courses/Master React JS/myapp/src/App.js
+++ Youtube-courses/Master React JS/myapp/src/App.js
@@ -1,11 +1,28 @@
 const Button = () => {
-  return <button>Click</button>
-}
+  const buttonStyle = {
+    background: "blue",
+    width: "200px",
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: "20px",
+    outline: "none",
+    boxShadow: "0px 0px 10px black",
+    cursor: "pointer"
+  };
+
+  const handleClick = (a, b) => {
+    console.log(a + b);
+    alert(`${a} + ${b} = ${a + b}`);
+  }
+
+  return <button style={buttonStyle} onClick={() => handleClick(2, 2)}>Click</button>
+};
 
 function App() {
+
   return (
     <>
-  <Button />
+      <Button />
     </>
   );
 }
