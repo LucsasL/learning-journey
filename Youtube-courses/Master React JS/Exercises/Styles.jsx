8413c79c9cfa5429ffc69f445f7b386cb5b4e1ed
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-24 13:16:09 -0300

    Variable CSS + Exterior Style

--- Youtube-courses/Master React JS/Exercises/Styles.jsx
+++ Youtube-courses/Master React JS/Exercises/Styles.jsx
@@ -1,11 +1,31 @@
-function Component() {
-  return (
-    <>
-      <h1 style={{color: "White", backgroundColor: "teal", padding: "20px"}}>
-        Inline Style
-      </h1>
-    </>
-  );
+function Style() {
+    const headerStyle = {
+      background: "#8000ff",
+      color: "black",
+      padding: "20px",
+      borderRadius: "20px",
+      fontFamily: "Arial, sans-serif",
+    };
+
+    return (
+      <>
+        {/* Inline style */}
+        <h1
+          style={{
+            fontFamily: "Arial, sans-serif",
+            color: "black",
+            backgroundColor: "blue",
+            padding: "20px",
+            borderRadius: "20px",
+          }}
+        >
+          Inline Style in React
+        </h1>
+
+        {/* Variable style */}
+        <h2 style={headerStyle}>Something here</h2>
+      </>
+    );
 }
 
-export default Component;
+export default Style;
--- Youtube-courses/Master React JS/myapp/src/App.js
+++ Youtube-courses/Master React JS/myapp/src/App.js
@@ -1,23 +1,32 @@
+import "./index.css";
+
 const Style = () => {
   const headerStyle = {
     background: "#8000ff",
     color: "black",
     padding: "20px",
-    borderRadius: "20px",
     fontFamily: "Arial, sans-serif",
   };
 
   return (
     <>
       {/* Inline style */}
-      <h1 style={{ fontFamily: "Arial, sans-serif", color: "black", backgroundColor: "blue", padding: "20px", borderRadius: "20px" }}>
+      <h1
+        style={{
+          fontFamily: "Arial, sans-serif",
+          color: "black",
+          backgroundColor: "blue",
+          padding: "20px",
+        }}
+      >
         Inline Style in React
       </h1>
 
       {/* Variable style */}
-      <h2 style={headerStyle}>
-        Something here
-      </h2>
+      <h2 style={headerStyle}>Something here</h2>
+
+      {/* Exterior Style */}
+      <h3>I'm fine with adding Styles</h3>
     </>
   );
 };
