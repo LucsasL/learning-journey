4e4bf7e27b1e4af033d0fd8f1f15b0955d40bbaf
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-24 14:27:40 -0300

    Styling with Tailwind CSS

--- Youtube-courses/Master React JS/myapp/src/App.js
+++ Youtube-courses/Master React JS/myapp/src/App.js
@@ -1,57 +1,7 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
-import Button from "react-bootstrap/Button";
-
-function Buttons() {
-  return (
-    <>
-      <Button variant="outline-primary">Primary</Button>
-      <Button variant="outline-secondary">Secondary</Button>
-      <Button variant="outline-success">Success</Button>
-      <Button variant="outline-warning">Warning</Button>
-      <Button variant="outline-danger">Danger</Button>
-      <Button variant="outline-info">Info</Button>
-      <Button variant="outline-light">Light</Button>
-      <Button variant="outline-dark">Dark</Button>
-    </>
-  );
-}
-
-function NavBarComponent() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
-
 function App() {
   return (
     <>
-      <NavBarComponent />
+      <h1 className="text-orange-800">Hello, world!</h1>
     </>
   );
 }
