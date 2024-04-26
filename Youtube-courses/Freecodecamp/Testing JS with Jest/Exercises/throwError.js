31bc1329f72cea41dfcea5f11e64a10b184b626d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-26 18:58:29 -0300

    Update throwError.js

--- Youtube-courses/Freecodecamp/Testing JS with Jest/Exercises/throwError.js
+++ Youtube-courses/Freecodecamp/Testing JS with Jest/Exercises/throwError.js
@@ -2,9 +2,12 @@ function myFunction(invalidInputParam) {
   try {
     invalidInputParam += "Will it work? Nah.";
     return invalidInputParam;
+    
   } catch (error) {
     throw new Error("This error is being thrown due to invalid input.");
   }
 }
 
-invalidInput = 0;
+let invalidInput = 0;
+
+module.exports = myFunction;
