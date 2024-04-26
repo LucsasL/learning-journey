701f7bc9e9f4e1cc40b9586fd1cda9ae11401041
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-26 18:19:34 -0300

    toEqual

--- Youtube-courses/Freecodecamp/Testing JS with Jest/Exercises/equalTo.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require("./sum");
-
-test("it has to be equal to", () => {
-  expect(2 + 2).toEqual(2 * 2);
-});
---
+++ Youtube-courses/Freecodecamp/Testing JS with Jest/Exercises/toEqual.test.js
@@ -0,0 +1,6 @@
+test("Object Assignment", () => {
+  const data = {one: 1};
+  data["two"] = 2;
+
+  expect(data).toEqual({ one: 1, two: 2 });
+});
