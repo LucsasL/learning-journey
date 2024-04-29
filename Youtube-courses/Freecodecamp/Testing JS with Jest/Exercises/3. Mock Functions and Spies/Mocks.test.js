b60999fa2580c51c9d2ac8ba254fe17ed6a86590
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-04-28 22:43:39 -0300

    Mocks

--- Youtube-courses/Freecodecamp/Testing JS with Jest/Exercises/3. Mock Functions and Spies/Mocks.test.js
+++ Youtube-courses/Freecodecamp/Testing JS with Jest/Exercises/3. Mock Functions and Spies/Mocks.test.js
@@ -1,1 +1,8 @@
-require("./Mocks");
+test("Mock Implementation of a basic function", () => {
+  const mock = jest.fn((x) => 42 + x);
+
+  expect(mock(1)).toBe(43);
+
+  // toHaveBeenCalledWith() Method: It checks if the function were called in the way the parameters say it was.
+  expect(mock).toHaveBeenCalledWith(1);
+});
