f2a53fda15706e6de1a052c9a4af88510df1b64d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-07 01:43:23 -0300

    Update Forms.jsx

--- Youtube-courses/HuXn WebDev/Master React JS/Exercises/Forms.jsx
+++ Youtube-courses/HuXn WebDev/Master React JS/Exercises/Forms.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 const Forms = () => {
+  const [username, setUsername] = useState("");
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`You typed: ${username}`);
+    setUsername("");
+  };
+
   return (
     <>
-    
+      <h1>Form Demo</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input value={username} onChange={handleChange} type="text" />
+        <input type="submit" value="Submit" />
+      </form>
     </>
-  )
-}
+  );
+};
 
 export default Forms;
