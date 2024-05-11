06bfd3ece8acd970612a2b7974351a41145b5d3e
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-10 23:43:31 -0300

    Update App.js

--- Youtube-courses/HuXn WebDev/Master React JS/myapp/src/App.js
+++ Youtube-courses/HuXn WebDev/Master React JS/myapp/src/App.js
@@ -1,4 +1,12 @@
+import { useReducer } from "react";
+
+// state: It's the state of our application
+// dispatch: [value setValue]
+
+// reducer: 
+
 const App = () => {
+  const [ state, dispatch ] = useReducer(reducer, initialState);
 
   return (
     <>
