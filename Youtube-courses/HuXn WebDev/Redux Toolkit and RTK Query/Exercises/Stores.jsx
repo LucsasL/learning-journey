8ca572e5194ae2185fb0e9dca8583f153428165f
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-15 08:39:51 -0300

    Create Stores.jsx

---
+++ Youtube-courses/HuXn WebDev/Redux Toolkit and RTK Query/Exercises/Stores.jsx
@@ -0,0 +1,4 @@
+// What is a Store?
+
+// A store is a central place where the state of our application is stored & it can be created using the configureStore function, and it holds the entire state tree of our application.
+
