4a9662ab80d10bc6d20d8eca7610973393b35c38
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-16 01:37:19 -0300

    Setting things up

---
+++ Youtube-courses/HuXn WebDev/Redux Toolkit and RTK Query/Exercises/Redux Toolkit Query.jsx
@@ -0,0 +1,15 @@
+// Redux Toolkit Query
+
+// Redux Toolkit Query (RTK) is specifically designed to simplify data fetching, caching, and state management for API calls in React and Redux application.
+
+// What We'll Learn?
+
+// -> How to Get all Products
+
+// -> How to Add a specific product
+
+// -> How to Add a new product
+
+// -> How to Update a product
+
+// -> How to Delete a product
