9a705923a7f5e4b3d343ed719c4711c230579c51
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-21 15:41:01 -0300

    Meal API Project

---
+++ Youtube-courses/HuXn WebDev/Master React JS/Projects/mealapi/src/components/Main.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "../styles/style.css";
+import axios from "axios";
+
+const Main = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+    .then(r => {
+      console.log(r);
+      setItems(r.data.meals);
+    })
+    .catch(err => console.log(err));
+  }, []);
+
+  const itemsList = items.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className="card" key={idMeal}>
+        <h1>{strMeal}</h1>
+        <img src={strMealThumb} alt={strMeal} />
+
+        <div className="content">
+          <p>{strMeal}</p>
+
+          <p>#{idMeal}</p>
+        </div>
+      </section>
+    );
+  });
+
+  return (
+    <div className="itemsCont">
+      {itemsList}
+    </div>
+  );
+}
+
+export default Main;
