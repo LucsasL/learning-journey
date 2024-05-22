18c5b017adde8487d555e7da2c4c4be00d103a94
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-22 12:56:30 -0300

    Update Calculator.jsx

--- Youtube-courses/HuXn WebDev/Master React JS/myapp/src/components/Calculator.jsx
+++ Youtube-courses/HuXn WebDev/Master React JS/myapp/src/components/Calculator.jsx
@@ -1,18 +1,25 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "../styles/style.css";
 
 const Calculator = () => {
   const [inputValue, setInputValue] = useState("");
 
-  useEffect(display, );
-
-  function display(value) {
+  function display(value, e) {
+    e.preventDefault();
     setInputValue(inputValue + value);
   }
 
-  function calculate() {
+  function calculate(e) {
+    e.preventDefault();
     let answers = eval(inputValue);
     setInputValue(answers);
+  }
+  
+  function eraseDigit(value, e) {
+    e.preventDefault();
+    let chars = value.split("");
+    chars.pop();
+    setInputValue(chars.join(""));
   }
 
   function clear() {
@@ -22,78 +29,83 @@ const Calculator = () => {
   return (
     <>
       <form name="calc" className="calculator">
-        <input type="text" className="value" value={inputValue}></input>
+        <input 
+          type="text" 
+          className="value" 
+          value={inputValue}
+          onChange={inp => display(inp)}
+        ></input>
 
         <div>
-          <button>
+          <button onClick={(e) => clear()}>
             C
           </button>
 
-          <button onClick={() => clear()}>
-            CE
+          <button onClick={(e) => clear()}>
+            C
           </button>
 
-          <button>
+          <button onClick={(e) => eraseDigit(inputValue, e)}>
             &lt;
           </button>
 
-          <button onClick={() => display("*")}>
+          <button onClick={(e) => display("*", e)}>
             *
           </button>
 
-          <button onClick={() => display("1")}>
+          <button onClick={(e) => display("1", e)}>
             1
           </button>
 
-          <button onClick={() => display("2")}>
+          <button onClick={(e) => display("2", e)}>
             2
           </button>
 
-          <button onClick={() => display("3")}>
+          <button onClick={(e) => display("3", e)}>
             3
           </button>
 
-          <button onClick={() => display("/")}>
+          <button onClick={(e) => display("/", e)}>
             /
           </button>
 
-          <button onClick={() => display("4")}>
+          <button onClick={(e) => display("4", e)}>
             4
           </button>
 
-          <button onClick={() => display("5")}>
+          <button onClick={(e) => display("5", e)}>
             5
           </button>
 
-          <button onClick={() => display("6")}>
+          <button onClick={(e) => display("6", e)}>
             6
           </button>
 
-          <button onClick={() => display("-")}>
+          <button onClick={(e) => display("-", e)}>
             -
           </button>
 
-          <button onClick={() => display("7")}>
+          <button onClick={(e) => display("7", e)}>
             7
           </button>
 
-          <button onClick={() => display("8")}>
+          <button onClick={(e) => display("8", e)}>
             8
           </button>
 
-          <button onClick={() => display("9")}>
+          <button onClick={(e) => display("9", e)}>
             9
           </button>
 
-          <button onClick={() => display("+")}>
+          <button onClick={(e) => display("+", e)}>
             +
           </button>
 
-          <button onClick={() => display(".")}>
+          <button onClick={(e) => display(".", e)}>
             .
           </button>
 
-          <button onClick={() => display("0")}>
+          <button onClick={(e) => display("0", e)}>
             0
           </button>
 
@@ -101,7 +113,7 @@ const Calculator = () => {
             %
           </button>
 
-          <button onClick={() => calculate()}>
+          <button onClick={(e) => calculate(e)}>
             =
           </button>
         </div>
