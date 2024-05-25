72eb18c6eac2b61131eeca3d7b5ebec7f0dbd820
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-25 20:27:42 -0300

    Fixing the calculator project

--- Youtube-courses/HuXn WebDev/Master React JS/Projects/calculator/src/components/Calculator.jsx
+++ Youtube-courses/HuXn WebDev/Master React JS/Projects/calculator/src/components/Calculator.jsx
@@ -28,96 +28,78 @@ const Calculator = () => {
 
   return (
     <>
-      <form name="calc" className="calculator">
-        <input 
-          type="text" 
-          className="value" 
-          value={inputValue}
-          onChange={inp => display(inp)}
-        ></input>
-
-        <div>
-          <button onClick={(e) => clear()}>
-            C
-          </button>
-
-          <button onClick={(e) => clear()}>
-            C
-          </button>
-
-          <button onClick={(e) => eraseDigit(inputValue, e)}>
-            &lt;
-          </button>
-
-          <button onClick={(e) => display("*", e)}>
-            *
-          </button>
-
-          <button onClick={(e) => display("1", e)}>
-            1
-          </button>
-
-          <button onClick={(e) => display("2", e)}>
-            2
-          </button>
-
-          <button onClick={(e) => display("3", e)}>
-            3
-          </button>
-
-          <button onClick={(e) => display("/", e)}>
-            /
-          </button>
-
-          <button onClick={(e) => display("4", e)}>
-            4
-          </button>
-
-          <button onClick={(e) => display("5", e)}>
-            5
-          </button>
-
-          <button onClick={(e) => display("6", e)}>
-            6
-          </button>
-
-          <button onClick={(e) => display("-", e)}>
-            -
-          </button>
-
-          <button onClick={(e) => display("7", e)}>
-            7
-          </button>
-
-          <button onClick={(e) => display("8", e)}>
-            8
-          </button>
-
-          <button onClick={(e) => display("9", e)}>
-            9
-          </button>
-
-          <button onClick={(e) => display("+", e)}>
-            +
-          </button>
-
-          <button onClick={(e) => display(".", e)}>
-            .
-          </button>
-
-          <button onClick={(e) => display("0", e)}>
-            0
-          </button>
-
-          <button>
-            %
-          </button>
-
-          <button onClick={(e) => calculate(e)}>
-            =
-          </button>
-        </div>
-      </form>
+      <section>
+        <form name="calc" className="calculator">
+          <input
+            type="text"
+            className="value"
+            value={inputValue}
+            onChange={inp => display(inp)}
+          ></input>
+          <div>
+            <button onClick={(e) => clear()}>
+              C
+            </button>
+            <button onClick={(e) => clear()}>
+              C
+            </button>
+            <button onClick={(e) => eraseDigit(inputValue, e)}>
+              &lt;
+            </button>
+            <button onClick={(e) => display("*", e)}>
+              *
+            </button>
+            <button onClick={(e) => display("1", e)}>
+              1
+            </button>
+            <button onClick={(e) => display("2", e)}>
+              2
+            </button>
+            <button onClick={(e) => display("3", e)}>
+              3
+            </button>
+            <button onClick={(e) => display("/", e)}>
+              /
+            </button>
+            <button onClick={(e) => display("4", e)}>
+              4
+            </button>
+            <button onClick={(e) => display("5", e)}>
+              5
+            </button>
+            <button onClick={(e) => display("6", e)}>
+              6
+            </button>
+            <button onClick={(e) => display("-", e)}>
+              -
+            </button>
+            <button onClick={(e) => display("7", e)}>
+              7
+            </button>
+            <button onClick={(e) => display("8", e)}>
+              8
+            </button>
+            <button onClick={(e) => display("9", e)}>
+              9
+            </button>
+            <button onClick={(e) => display("+", e)}>
+              +
+            </button>
+            <button onClick={(e) => display(".", e)}>
+              .
+            </button>
+            <button onClick={(e) => display("0", e)}>
+              0
+            </button>
+            <button>
+              %
+            </button>
+            <button onClick={(e) => calculate(e)}>
+              =
+            </button>
+          </div>
+        </form>
+      </section>
     </>
   )
 }
