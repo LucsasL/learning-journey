7d37426d362c9e1b437d217e60ed8d48a09e1fbe
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-29 23:34:46 -0300

    Path Modules in NodeJS

--- Youtube-courses/HuXn WebDev/Complete Node JS Bootcamp/Exercises/4. Path Module/index.js
+++ Youtube-courses/HuXn WebDev/Complete Node JS Bootcamp/Exercises/4. Path Module/index.js
@@ -1,7 +1,40 @@
 // Path Module
 import path from "path";
 
-// c:\\courses\\nodejs\\path-module
-console.log(path.basename());
+// path.basename(pathString) method
 
-// Work in Progress
+// It takes the name of the file from the path and then returns it
+console.log(path.basename("c:/nodejs/index.js"));
+console.log(path.basename("c:/nodejs/index.js", ".js"));
+
+// path.dirname(pathString) method
+
+// It takes the name of the directories and then returns it
+console.log(path.dirname("c:/nodejs/index.js"));
+
+// path.extname(pathString) method
+
+// It takes the extension name of a file and then returns it
+console.log(path.extname("c:/nodejs/index.js"));
+
+// path.join(string1, string2, string3) method
+
+// It takes two strings and join them in one, returning a path
+console.log(path.join("c:", "irineu-nusabi", "HTML&CSS"));
+
+// You can up a level in the path, ignoring the last directory you specified
+console.log(path.join("c:", "irineu-nusabi", "HTML&CSS", ".."));
+console.log(path.join("c:", "irineu-nusabi", "HTML&CSS", "..", ".."));
+
+// path.normalize(pathString) method
+
+// It takes a path and returns the path formatted in the right way
+console.log(path.normalize("c:///courses///redux///store//features//"));
+
+// path.parse() method
+
+// It returns an object whose properties represent significant elements of the path. (root, dir, base, ext, name)
+console.log(path.parse("c:/nodejs/courses/app.js"));
+console.log(path.parse("c:/nodejs/courses/app.js").dir);
+console.log(path.parse("c:/nodejs/courses/app.js").base);
+console.log(path.parse("c:/nodejs/courses/app.js").ext);
