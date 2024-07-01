41e7d49d4e58bbc6fa7b5e08037e4580350d05ee
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-30 22:14:31 -0300

    ...

--- Youtube-courses/HuXn WebDev/Complete Node JS Bootcamp/playground/index.js
+++ Youtube-courses/HuXn WebDev/Complete Node JS Bootcamp/playground/index.js
@@ -4,11 +4,10 @@
 import * as fs from "fs/promises";
 
 try {
-  // const bruh = await fs.mkdir("c:/nodejs/courses/redux/toolkit", { recursive: true });
+  // const bruh = await fs.mkdir("c:\nodejs\courses/redux/toolkit", { recursive: true });
   console.log("Folder created.");
 } catch (error) {
   console.log(error);
 }
-
 
 // It's the code of the last exercise I did in the course
