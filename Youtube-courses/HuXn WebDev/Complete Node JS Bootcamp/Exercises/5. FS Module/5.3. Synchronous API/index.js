3b7885ae97f5b6c6de5b460dd1e05cf499fef4f6
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-07-01 23:35:53 -0300

    Synchronous way of using FS Module

--- Youtube-courses/HuXn WebDev/Complete Node JS Bootcamp/Exercises/5. FS Module/5.3. Synchronous API/index.js
+++ Youtube-courses/HuXn WebDev/Complete Node JS Bootcamp/Exercises/5. FS Module/5.3. Synchronous API/index.js
@@ -1,1 +1,11 @@
 // FileSystem Module
+import * as fs from "fs";
+
+// use the synchronous version of the methods adding "Sync" at the end of the method name
+
+// Create a directory
+fs.mkdirSync("c:/nodejs/test/demo", { recursive: true });
+
+// Remove Directory
+fs.rmSync("c:/nodejs", { recursive: true });
+console.log("Done.");
