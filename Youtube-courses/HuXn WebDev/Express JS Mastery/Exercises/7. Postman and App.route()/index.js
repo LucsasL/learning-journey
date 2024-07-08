fc9c9d5f1c750aaee4c158af7f3205f09b2da705
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-07-08 20:22:33 -0300

    app.route Method

--- Youtube-courses/HuXn WebDev/Express JS Mastery/Exercises/7. Postman and App.route()/index.js
+++ Youtube-courses/HuXn WebDev/Express JS Mastery/Exercises/7. Postman and App.route()/index.js
@@ -3,21 +3,40 @@ import express from "express";
 const app = express();
 
 // Ugly Code
-app.get("/student", (req, res) => {
-  res.send("All Students");
-});
+// app.get("/student", (req, res) => {
+//   console.log("Get method used.");
+//   res.send("All Students");
+// });
 
-app.post("/student", (req, res) => {
-  console.log("Post method");
-  res.send("Add New Student");
-});
+// app.post("/student", (req, res) => {
+//   console.log("Post method used.");
+//   res.send("Add New Student");
+// });
 
-app.put("/student", (req, res) => {
-  res.send("Update Students");
-});
+// app.put("/student", (req, res) => {
+//   console.log("Put method used.");
+//   res.send("Update Students");
+// });
 
-app.delete("/student", (req, res) => {
-  res.send("Update Students");
-});
+// app.delete("/student", (req, res) => {
+//   console.log("Delete method used.");
+//   res.send("Update Students");
+// });
+
+// Refactoring code
+app
+  .route("/student")
+  .get((req, res) => {
+    res.send("All students.");
+  })
+  .post((req, res) => {
+    res.send("Add New Student");
+  })
+  .put((req, res) => {
+    res.send("Update Student");
+  })
+  .delete((req, res) => {
+    res.send("Delete Student");
+  });
 
 app.listen(8000, () => console.log("Listening server on port 8000"));
