767cb27ebc73b9b74f3c760769227fb384b438c9
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-07-09 01:15:17 -0300

    Params in Expres Done

--- Youtube-courses/HuXn WebDev/Express JS Mastery/Exercises/9. Route Params/index.js
+++ Youtube-courses/HuXn WebDev/Express JS Mastery/Exercises/9. Route Params/index.js
@@ -10,4 +10,15 @@ app.get("/product/order/:day/:month/:year", (req, res) => {
   res.send(`Product was ordered on: ${month} / ${day} / ${year}`);
 })
 
+// app.param() Method
+app.param("id", (req, res, next, id) => {
+  console.log(`ID: ${id}.`);
+  next();
+});
+
+app.get("/user/:id", (req, res) => {
+  console.log(`This is user ID Path`);
+  res.send("Responsee Ok");
+});
+
 app.listen(8000, () => console.log("Listening server on port 8000"));
