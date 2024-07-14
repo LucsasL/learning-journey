2ac02efc5ce866a699eb377ea799c07a6211201e
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-07-14 08:48:39 -0300

    Query String (Now it's legit)

--- Youtube-courses/HuXn WebDev/Express JS Mastery/Exercises/11. Query Strings/index.js
+++ Youtube-courses/HuXn WebDev/Express JS Mastery/Exercises/11. Query Strings/index.js
@@ -6,12 +6,12 @@ const app = express();
 
 // A Query String is the part of a url that is used to pass data from the client side to the server side of an application
 
-// Appenders come to add more queries to the URL
+// Ampersand come to add more queries to the URL, you can add inumerable ampersands to add more and more query strings to your URL
 
 app.get("/product", (req, res) => {
   const { category, id } = req.query;
 
-  res.send(`Product Category: ${category}\n ID: ${id}`);
+  res.send(`Product Category: ${category}\n& ID: ${id}`);
 });
 
 app.listen(8000, () => console.log("Listening server at port 8000"));
