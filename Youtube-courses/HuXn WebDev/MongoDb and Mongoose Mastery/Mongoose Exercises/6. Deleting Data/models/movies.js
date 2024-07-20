9f049cc7064969dd5d8a1c8d5d03208feee0114b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-07-20 17:17:33 -0300

    MongoDB and Mongoose Course Finished

---
+++ Youtube-courses/HuXn WebDev/MongoDb and Mongoose Mastery/Mongoose Exercises/6. Deleting Data/models/movies.js
@@ -0,0 +1,46 @@
+// Importing Mongoose
+import mongoose, { mongo } from "mongoose";
+
+// Defining the Movie Schema
+const movieSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  ratings: { type: Number, required: true, min: 1, max: 5 },
+  money: {
+    type: mongoose.Decimal128,
+    required: true,
+    validate: (v) => v >= 10,
+  },
+  isActive: { type: Boolean },
+  comments: [
+    { value: { type: String }, pubished: { type: Date, default: Date.now } },
+  ],
+});
+
+// Creating the Movie Model
+const MovieModel = mongoose.model("Movie", movieSchema);
+
+// Function that deletes all the data
+const allDocs = async () => {
+  try {
+    const result = await MovieModel.deleteMany({}); // All data
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Function that deletes a single document
+const singleDoc = async () => {
+  try {
+    // const result = await MovieModel.findByIdAndDelete("669bcf1cc0abefe708eb0414"); // Single data
+    const result = await MovieModel.deleteOne({ name: "Movie: The Return" }); // Single data
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Exporting the Function
+export { allDocs, singleDoc };
