a5d2b88de84cd81a9d8da2a3a0cb43671a8f6bc6
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-07-23 12:48:03 -0300

    useRouter() Hook

--- Youtube-courses/HuXn WebDev/Mastering Next JS/Exercises/02. Routing/2.4. useRouter()/index.jsx
+++ Youtube-courses/HuXn WebDev/Mastering Next JS/Exercises/02. Routing/2.4. useRouter()/index.jsx
@@ -5,3 +5,38 @@
 // It provides programmatic control over the router and allows you to navigate to different pages or perform other routing-related actions in response to user interactions or events within you component.
 
 // It's useful when you need to handle navigation or access route-specific information directly in your component logic.
+
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+function Home() {
+  const router = useRouter();
+
+  const navigate = (page) => {
+    router.push(page);
+  };
+
+  return (
+    <>
+      <section>
+        <h1>useRouter()</h1>
+        <button className="border p-2 m-2" onClick={() => navigate("about")}>
+          Go to about page
+        </button>{" "}
+        <br />
+        <button className="border p-2 m-2" onClick={() => navigate("login")}>
+          Go to login page
+        </button>{" "}
+        <br />
+        <button className="border p-2 m-2" onClick={() => navigate("projects")}>
+          Go to the projects page
+        </button>{" "}
+        <br />
+      </section>
+    </>
+  );
+}
+
+export default Home;
--- Youtube-courses/HuXn WebDev/Mastering Next JS/next-course/app/page.js
+++ Youtube-courses/HuXn WebDev/Mastering Next JS/next-course/app/page.js
@@ -1,11 +1,43 @@
-import React from 'react';
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
 
 function Home() {
+  const router = useRouter();
+
+  const navigate = (page) => {
+    router.push(page);
+  }
+
   return (
     <>
-      <div>Home</div>
+      <section>
+        <h1>useRouter()</h1>
+
+        <button
+          className="border p-2 m-2"
+          onClick={() => navigate("about")}
+        >
+          Go to about page
+        </button> <br />
+
+        <button
+          className="border p-2 m-2"
+          onClick={() => navigate("login")}
+        >
+          Go to login page
+        </button> <br />
+
+        <button
+          className="border p-2 m-2"
+          onClick={() => navigate("projects")}
+        >
+          Go to the projects page
+        </button> <br />
+      </section>
     </>
-  )
+  );
 }
 
 export default Home;
