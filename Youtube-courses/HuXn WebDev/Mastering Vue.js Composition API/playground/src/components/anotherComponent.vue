501fd340bc9d1782232657b7123a279aaa3ed228
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-08-01 19:09:09 -0300

    Nested Components in Vue

---
+++ Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/playground/src/components/anotherComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>This is another components, it's nested</h1>
+
+  <p>
+    This is some extra content from the children component.
+  </p>
+</template>
--- Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/playground/src/components/helloWorld.vue
+++ Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/playground/src/components/helloWorld.vue
@@ -1,11 +1,22 @@
 <!-- JS -->
 <script setup>
+  import anotherComponent from './anotherComponent.vue';
   console.log("Hello world!");
 </script>
 
 <!-- HTML -->
 <template>
   <h1>Hello world!</h1>
+
+  <h2>
+    This is a Parent Component
+  </h2>
+
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque culpa minima iusto illum, dignissimos fuga modi ab deleniti repellat nemo distinctio laborum a magnam possimus ullam porro natus eligendi veniam.
+  </p>
+
+  <anotherComponent />
 </template>
 
 <!-- CSS -->
