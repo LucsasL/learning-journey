35f315c59e4fc44f984cd83644afb5a4e812778b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-08-01 13:08:41 -0300

    Fetching data from the client

--- Youtube-courses/HuXn WebDev/Mastering Next JS/Exercises/06. Fetch Data in Client/index.jsx
+++ Youtube-courses/HuXn WebDev/Mastering Next JS/Exercises/06. Fetch Data in Client/index.jsx
@@ -1,1 +1,41 @@
 // Fetching data from the client
+
+"use client";
+
+import React from 'react'
+import { useState, useEffect } from "react";
+
+function FetchingDataClient() {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let data = await fetch("https://jsonplaceholder.typicode.com/posts");
+      data = await data.json();
+
+      console.log(data);
+      setProduct(data);
+    }
+
+    fetchData();
+
+  }, []);
+
+  return (
+    <>
+      <section>
+        <h1>
+          This data is from an external API.
+        </h1>
+
+        <ul>
+          {product.map((p) => {
+            return <li key={p.id}>{p.title}</li>;
+          })}
+        </ul>
+      </section>
+    </>
+  );
+}
+
+export default FetchingDataClient;
--- Youtube-courses/HuXn WebDev/Mastering Next JS/next-course/app/page.js
+++ Youtube-courses/HuXn WebDev/Mastering Next JS/next-course/app/page.js
@@ -2,14 +2,27 @@
 
 // Hooks Import
 import React from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 // Fonts Import
 import { Roboto } from "next/font/google";
-const roboto = new Roboto({ subsets: ["latin"], weight: "500" });
+const roboto = Roboto({ subsets: ["latin"], weight: "500" });
 
 function Home() {
   const router = useRouter();
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let data = await fetch("https://jsonplaceholder.typicode.com/posts");
+      data = await data.json();
+      setProduct(data);
+      console.log(data);
+    }
+
+    fetchData();
+  }, []);
 
   const navigate = (page) => {
     router.push(page);
@@ -21,6 +34,14 @@ function Home() {
         <h1 className={roboto}>Google Fonts</h1>
 
         <h2 className={roboto}>Hello, this is roboto font</h2>
+
+        <ul>
+          {
+            product.map(p => {
+              return <li key={p.id}>{p.title}</li>
+            })
+          }
+        </ul>
       </section>
     </>
   );
