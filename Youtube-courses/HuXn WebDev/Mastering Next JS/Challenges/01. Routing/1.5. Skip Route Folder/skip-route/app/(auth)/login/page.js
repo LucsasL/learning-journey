c345005b2f1cf10c9ee8038aa1e07a4cb7e3cbba
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-08-04 23:09:36 -0300

    Skiping routes in Next.js

---
+++ Youtube-courses/HuXn WebDev/Mastering Next JS/Challenges/01. Routing/1.5. Skip Route Folder/skip-route/app/(auth)/login/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  console.log("Please Login To See The Content");
+
+  return (
+    <>
+      <h1>Please Login To See The Content</h1>
+    </>
+  );
+}
+
+export default Login;
---
+++ Youtube-courses/HuXn WebDev/Mastering Next JS/Challenges/01. Routing/1.5. Skip Route Folder/skip-route/app/(users)/userinfo/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function UserInfo() {
+  return (
+    <>
+      <h1>
+        User info
+      </h1>
+
+      <p>Won't add anything bruh</p>
+    </>
+  )
+}
+
+export default UserInfo;
