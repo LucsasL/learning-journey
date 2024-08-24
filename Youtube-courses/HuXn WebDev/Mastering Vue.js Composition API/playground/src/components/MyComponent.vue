d7962581b27d78529857a0631984a64cd8c744be
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-08-24 04:03:12 -0300

    Applying images using Dynamic Binding

--- Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/playground/src/components/MyComponent.vue
+++ Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/playground/src/components/MyComponent.vue
@@ -1,7 +1,7 @@
 <!-- JS -->
 <script setup>
   const myGithub = "https://github.com/lucsasl";
-  const imgAlt = "idk what to add"
+  const imgAlt = "idk what to add";
 
 
   // -----------------------------------
@@ -9,16 +9,19 @@
 
   const imageInfo = {
     src: "https://images.unsplash.com/photo-1699646034253-0ebaa551d226?q=80&w=2070&auto=format&fit=crop&ixlib=M3wxMjA3fDB8MHxwa690by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Idk what to add here too"
   }
 </script>
 
 <!-- HTML -->
 <template>
   <!-- Old way -->
-  <a v-bind:href="myGithub" :alt="imgAlt" target="_blank">Lucsas's Github</a> <br />
+  <a v-bind:href="myGithub" v-bind:alt="imgAlt" target="_blank">Lucsas's Github</a> <br />
   
   <!-- New way -->
   <a :href="myGithub" :alt="imgAlt" target="_blank">Lucsas's Github</a>
+
+  <img :src="imageInfo.src" :alt="imageInfo.alt" />
 </template>
 
 <!-- CSS -->
