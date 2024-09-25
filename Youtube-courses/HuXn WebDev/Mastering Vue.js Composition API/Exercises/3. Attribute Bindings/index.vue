f9408ee772e70651f2b93014dbd322c6c7fce872
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-09-25 10:56:28 -0300

    Coming back to study Vue.js; Attributte Binding in Vue.js

--- Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/Exercises/3. Attribute Bindings/index.vue
+++ Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/Exercises/3. Attribute Bindings/index.vue
@@ -6,28 +6,48 @@
 
 <!-- Old way 👇 -->
 
-<!-- v-bind: attr -->
+<!-- v-bind: <attr> -->
 
 <!-- New way 👇 -->
 
-<!-- :attr -->
+<!-- :<attr> -->
 
 <!-- JS -->
 <script setup>
   const myGithub = "https://github.com/lucsasl";
-  const imgAlt = "idk what to add"
+
+  // -----------------------------------
+  // Dynamic Binding
+
+  const imageInfo = {
+    src: "https://images.unsplash.com/photo-1699646034253-0ebaa551d226?q=80&w=2070&auto=format&fit=crop&ixlib=M3wxMjA3fDB8MHxwa690by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Idk what to add here too",
+    width: "400px",
+    height: "400px",
+  }
+
+  const relText = "noopener noreferrer";
 </script>
 
 <!-- HTML -->
 <template>
-  <!-- Old way 👇 -->
-  <a v-bind:href="myGithub" :alt="imgAlt" target="_blank">Lucsas's Github</a> <br />
+  <!-- Old way -->
+  <a v-bind:href="myGithub" v-bind:rel="relText" target="_blank">Lucsas's Github</a> <br />
   
-  <!-- New way 👇 -->
-  <a :href="myGithub" :alt="imgAlt" target="_blank">Lucsas's Github</a>
+  <!-- New way -->
+  <a :href="myGithub" :rel="relText" target="_blank">Lucsas's Github</a>
+
+  <img :src="imageInfo.src" :alt="imageInfo.alt" :width="imageInfo.width" :height="imageInfo.height" />
 </template>
 
 <!-- CSS -->
 <style>
-  
+  body {
+    background: #060606;
+  }
+
+  a {
+    color: blue;
+    text-decoration: none;
+  }
 </style>
--- Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/playground/src/components/MyComponent.vue
+++ Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/playground/src/components/MyComponent.vue
@@ -1,27 +1,29 @@
 <!-- JS -->
 <script setup>
   const myGithub = "https://github.com/lucsasl";
-  const imgAlt = "idk what to add";
-
 
   // -----------------------------------
   // Dynamic Binding
 
   const imageInfo = {
     src: "https://images.unsplash.com/photo-1699646034253-0ebaa551d226?q=80&w=2070&auto=format&fit=crop&ixlib=M3wxMjA3fDB8MHxwa690by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Idk what to add here too"
+    alt: "Idk what to add here too",
+    width: "400px",
+    height: "400px",
   }
+
+  const relText = "noopener noreferrer";
 </script>
 
 <!-- HTML -->
 <template>
   <!-- Old way -->
-  <a v-bind:href="myGithub" v-bind:alt="imgAlt" target="_blank">Lucsas's Github</a> <br />
+  <a v-bind:href="myGithub" v-bind:rel="relText" target="_blank">Lucsas's Github</a> <br />
   
   <!-- New way -->
-  <a :href="myGithub" :alt="imgAlt" target="_blank">Lucsas's Github</a>
+  <a :href="myGithub" :rel="relText" target="_blank">Lucsas's Github</a>
 
-  <img :src="imageInfo.src" :alt="imageInfo.alt" />
+  <img :src="imageInfo.src" :alt="imageInfo.alt" :width="imageInfo.width" :height="imageInfo.height" />
 </template>
 
 <!-- CSS -->
