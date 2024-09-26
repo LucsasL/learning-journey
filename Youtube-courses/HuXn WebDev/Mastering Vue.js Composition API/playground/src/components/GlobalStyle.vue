dd87073af16f7921e0aee079a5bca2c85f67237c
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-09-26 18:48:43 -0300

    Dynamic Binding Done

---
+++ Youtube-courses/HuXn WebDev/Mastering Vue.js Composition API/playground/src/components/GlobalStyle.vue
@@ -0,0 +1,23 @@
+<!-- JS -->
+<script setup>
+
+</script>
+
+<!-- HTML -->
+<template>
+  <div>
+     
+  </div>
+</template>
+
+<!-- CSS -->
+<style>
+  body {
+    background: #060606;
+  }
+
+  a {
+    color: blue;
+    text-decoration: none;
+  }
+</style>
