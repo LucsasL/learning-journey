0a49189f6d892017c18a2d846e1621e73eafe840
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-10-12 03:52:40 -0300

    Three.js Concepts

---
+++ Youtube-courses/Zero To Mastery/Three.js 101 Crash Course/Exercises/0. Concepts/0.2. Three.js Fundamentals.js
@@ -0,0 +1,15 @@
+// Three.js Fundamentals
+
+// Structure of a Three.js Application
+
+// The main parts of a 3D application: The Scene, the Camera and the Renderer
+
+// - Scene: Is everything that can be seen in a 3D Graphical App / Interface
+
+//  -> It contains all the objects, lights, textures, meshs, backgrounds, materials and shape; It's conceptually everything that exist in a 3D graphical environment / interface
+
+// - Camera: What the determines what the viewwe / user see
+
+// - Renderer: Takes information from the scene and the camera to the viewer / user
+
+//  -> Method that generates a 3D Environment: Renderer.render(scene, camera)
