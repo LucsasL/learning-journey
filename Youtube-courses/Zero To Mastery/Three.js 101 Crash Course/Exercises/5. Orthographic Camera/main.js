4177b3c308595c4452139ab03d79d9c2ca02f72a
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-10-12 23:06:42 -0300

    Working with Orthographic Cameras in Three.js

--- Youtube-courses/Zero To Mastery/Three.js 101 Crash Course/Exercises/5. Orthographic Camera/main.js
+++ Youtube-courses/Zero To Mastery/Three.js 101 Crash Course/Exercises/5. Orthographic Camera/main.js
@@ -1,21 +1,28 @@
 import * as THREE from "three";
-import { OrbitControl } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-// Initializing a Scene
-const scene = new THREE.Scene();
+// Main Variables
+const aspectRatio = window.innerWidth / window.innerHeight;
 
+// Models and Materials
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: "blue" });
 
+// Mesh
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+// Creating a Scene
+const scene = new THREE.Scene();
 
 scene.add(cubeMesh);
 
-// Initializing a Camera
-const camera = new THREE.PerspectiveCamera(
-  35,
-  window.innerWidth / window.innerHeight,
-  0.5,
+// Initializing an Orthographic Camera
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1,
+  -1,
+  .5,
   200
 );
 
@@ -27,14 +34,15 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const controls = new OrbitControl(camera, canvas);
+const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.autoRotate = true;
 
 // Creating a render loop
 const renderloop = () => {
+  controls.update();
   renderer.render(scene, camera);
-  window.requestAnimationFrame();
+  window.requestAnimationFrame(renderloop);
 }
 
 renderloop();
--- Youtube-courses/Zero To Mastery/Three.js 101 Crash Course/playground/main.js
+++ Youtube-courses/Zero To Mastery/Three.js 101 Crash Course/playground/main.js
@@ -1,51 +1,51 @@
-// Importing everything in three.js as "THREE"
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-// Printing the three.js module into the console
 console.log(THREE);
 console.log(OrbitControls);
 
-// Declaring a scene
+// Initializing a Scene
 const scene = new THREE.Scene();
 
-// Creating a cube shape and basic material
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "blue" });
 
-// Creating a mesh, which is made using a shape and a mesh
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-// Adding the mesh to the scene
 scene.add(cubeMesh);
 console.log(scene);
 
-// Initializing a camera
+// Initializing a Camera
+// const camera = new THREE.PerspectiveCamera(
+//   35,
+//   window.innerWidth / window.innerHeight,
+//   0.5,
+//   200
+// );
 
-// You provide the Method with the following arguments: The field of view of the perspective camera, an aspect ratio of the scene and a definition of what can't be seen when it's too close or too far from the camera
-const camera = new THREE.PerspectiveCamera(
-  35,
-  window.innerWidth / window.innerHeight,
+const aspectRatio = window.innerWidth / window.innerHeight;
+
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1,
+  -1,
   0.5,
-  200
+  200,
 );
 
-// Changing position of the camera
 camera.position.z = 5;
 
 // Initializing a Renderer
 const canvas = document.querySelector(".threejs");
 const renderer = new THREE.WebGLRenderer({ canvas });
 
-// sets the size of the 3D environment
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Making an instance of OrbitControls Object
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.autoRotate = true;
 
-// To make the 3D environment feel alive and interactive, you'll need to create a loop that requests the frame rate and updates the scene continuously as the user browses through the app
 const renderloop = () => {
   controls.update();
   renderer.render(scene, camera);
