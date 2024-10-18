1f12ae076cc62979ab24012c4618d05d562007e3
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-10-18 06:14:48 -0300

    Finishing three.js course

--- Youtube-courses/Zero To Mastery/Three.js 101 Crash Course/playground/main.js
+++ Youtube-courses/Zero To Mastery/Three.js 101 Crash Course/playground/main.js
@@ -1,160 +1,251 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-// Main Variables
+// Main Variablesb
 const aspectRatio = window.innerWidth / window.innerHeight;
 const maxPixelRatio = Math.min(window.devicePixelRatio, 2);
 
-// Creating a Scene
-const scene = new THREE.Scene();
+// Position of mercury in x axis
+let posx = 5;
 
 // Texture Loader
 const textureLoader = new THREE.TextureLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+cubeTextureLoader.setPath("textures2/cubeMap/");
 
-// Geometries and Materials
+// Textures
+const sunTex = textureLoader.load("textures2/2k_sun.jpg");
+sunTex.colorSpace = THREE.SRGBColorSpace;
+
+const mercuryTex = textureLoader.load("textures2/2k_mercury.jpg");
+mercuryTex.colorSpace = THREE.SRGBColorSpace;
+
+const venusTex = textureLoader.load("textures2/2k_venus_surface.jpg");
+venusTex.colorSpace = THREE.SRGBColorSpace;
+
+const earthTex = textureLoader.load("textures2/2k_earth_daymap.jpg");
+earthTex.colorSpace = THREE.SRGBColorSpace;
+
+const marsTex = textureLoader.load("textures2/2k_mars.jpg");
+marsTex.colorSpace = THREE.SRGBColorSpace;
+
+const moonTex = textureLoader.load("textures2/2k_moon.jpg");
+moonTex.colorSpace = THREE.SRGBColorSpace;
+
+
+// Geometries
 const sphereGeo = new THREE.SphereGeometry(1, 32, 32);
-const sphereUv = new THREE.BufferAttribute(sphereGeo.attributes.uv.array, 2);
-sphereGeo.setAttribute("uv2", sphereUv);
 
-// Grass Texture
-const grassTex = textureLoader.load(
-  "textures/whispy-grass-meadow-bl/wispy-grass-meadow_albedo.png"
-);
-grassTex.colorSpace = THREE.SRGBColorSpace;
-
-const grassAo = textureLoader.load(
-  "textures/whispy-grass-meadow-bl/wispy-grass-meadow_ao.png"
-);
-
-const grassHeight = textureLoader.load(
-  "textures/whispy-grass-meadow-bl/wispy-grass-meadow_height.png"
-);
-
-const grassMetalic = textureLoader.load(
-  "textures/whispy-grass-meadow-bl/wispy-grass-meadow_metalic.png"
-);
-
-const grassNormal = textureLoader.load(
-  "textures/whispy-grass-meadow-bl/wispy-grass-meadow_normal.png"
-);
-
-const grassRoughness = textureLoader.load(
-  "textures/whispy-grass-meadow-bl/wispy-grass-meadow_roughness.png"
-);
-
-// Badlands Texture
-const badlandsTex = textureLoader.load(
-  "textures/badlands-boulders-bl/badlands-boulders_albedo.png"
-);
-badlandsTex.colorSpace = THREE.SRGBColorSpace;
-
-const badlandsAo = textureLoader.load(
-  "textures/badlands-boulders-bl/badlands-boulders_ao.png"
-);
-
-const badlandsHeight = textureLoader.load(
-  "textures/badlands-boulders-bl/badlands-boulders_height.png"
-);
-
-const badlandsMetalic = textureLoader.load(
-  "textures/badlands-boulders-bl/badlands-boulders_metalic.png"
-);
-
-const badlandsNormal = textureLoader.load(
-  "textures/badlands-boulders-bl/badlands-boulders_normal.png"
-);
-
-const badlandsRoughness = textureLoader.load(
-  "textures/badlands-boulders-bl/badlands-boulders_roughness.png"
-);
-
-// Metal Texture
-const metalTex = textureLoader.load(
-  "textures/space-cruiser-panels2-bl/space-cruiser-panels2_albedo.png"
-);
-metalTex.colorSpace = THREE.SRGBColorSpace;
-
-const metalAo = textureLoader.load(
-  "textures/space-cruiser-panels2-bl/space-cruiser-panels2_ao.png"
-);
-
-const metalHeight = textureLoader.load(
-  "textures/space-cruiser-panels2-bl/space-cruiser-panels2_height.png"
-);
-
-const metalMetalic = textureLoader.load(
-  "textures/space-cruiser-panels2-bl/space-cruiser-panels2_metalic.png"
-);
-
-const metalNormal = textureLoader.load(
-  "textures/space-cruiser-panels2-bl/space-cruiser-panels2_normal.png"
-);
-
-const metalRoughness = textureLoader.load(
-  "textures/space-cruiser-panels2-bl/space-cruiser-panels2_roughness.png"
-);
-
-// Grass Material
-const grassMat = new THREE.MeshStandardMaterial({
-  map: grassTex,
-  roughnessMap: grassRoughness,
-  metalnessMap: grassMetalic,
-  normalMap: grassNormal,
-  displacementMap: grassHeight,
-  displacementScale: 0.1,
+// Planet Materials
+const sunMat = new THREE.MeshBasicMaterial({
+  map: sunTex,
 });
-grassMat.aoMap = grassAo;
 
-// Badlands Material
-const badlandsMat = new THREE.MeshStandardMaterial({
-  map: badlandsTex,
-  roughnessMap: badlandsRoughness,
-  metalnessMap: badlandsMetalic,
-  normalMap: badlandsNormal,
-  displacementMap: badlandsHeight,
-  displacementScale: 0.1,
+const mercuryMat = new THREE.MeshStandardMaterial({
+  map: mercuryTex,
 });
-badlandsMat.aoMap = badlandsAo;
 
-// Metal Material
-const metalMat = new THREE.MeshStandardMaterial({
-  map: metalTex,
-  roughnessMap: metalRoughness,
-  metalnessMap: metalMetalic,
-  normalMap: metalNormal,
-  displacementMap: metalHeight,
-  displacementScale: 0.1,
+const venusMat = new THREE.MeshStandardMaterial({
+  map: venusTex,
 });
-metalMat.aoMap = metalAo;
 
-// Initializing a Group
-const sphereGroup = new THREE.Group();
+const earthMat = new THREE.MeshStandardMaterial({
+  map: earthTex,
+});
 
-// Initializing the Meshes
-const grassSphere = new THREE.Mesh(sphereGeo, grassMat);
+const marsMat = new THREE.MeshStandardMaterial({
+  map: marsTex,
+});
 
-const badlandsSphere = new THREE.Mesh(sphereGeo, badlandsMat);
-badlandsSphere.position.set(2.5, 0, 0);
+// const jupiterMat = new THREE.MeshStandardMaterial({
+//   color: "lightbrown",
+// });
 
-const metalSphere = new THREE.Mesh(sphereGeo, metalMat);
-metalSphere.position.set(-2.5, 0, 0);
+// const saturnMat = new THREE.MeshStandardMaterial({
+//   color: "brown",
+// });
 
-// Adding spheres to the group
-sphereGroup.add(grassSphere, badlandsSphere, metalSphere);
-scene.add(sphereGroup);
+// const uranusMat = new THREE.MeshStandardMaterial({
+//   color: "lightblue",
+// });
 
-// Initializing lights
-const light = new THREE.AmbientLight(0xffffff, 1);
-scene.add(light);
+// const neptuneMat = new THREE.MeshStandardMaterial({
+//   color: "purple",
+// });
 
-const pointLight = new THREE.PointLight(0xffffff, 200);
-pointLight.position.set(5, 5, 5);
-scene.add(pointLight);
+// Moon Materials
+const moonMat = new THREE.MeshStandardMaterial({
+  map: moonTex,
+});
+
+// Initializing a scene
+const scene = new THREE.Scene();
+
+// Initializing Planets Meshes
+const sun = new THREE.Mesh(sphereGeo, sunMat);
+sun.scale.setScalar(5);
+
+// Planets Array
+const planets = [
+  {
+    name: "Mercury",
+    radius: 0.5,
+    distance: 10,
+    speed: 0.01,
+    material: mercuryMat,
+    moons: [],
+  },
+  {
+    name: "Venus",
+    radius: 0.8,
+    distance: 15,
+    speed: 0.007,
+    material: venusMat,
+    moons: [],
+  },
+  {
+    name: "Earth",
+    radius: 1,
+    distance: 20,
+    speed: 0.005,
+    material: earthMat,
+    moons: [
+      {
+        name: "Moon",
+        radius: 0.3,
+        distance: 2,
+        speed: 0.015,
+      },
+    ],
+  },
+  {
+    name: "Mars",
+    radius: 0.7,
+    distance: 25,
+    speed: 0.003,
+    material: marsMat,
+    moons: [
+      {
+        name: "Phobos",
+        radius: 0.1,
+        distance: 2,
+        speed: 0.02,
+      },
+      {
+        name: "Deimos",
+        radius: 0.2,
+        distance: 3,
+        speed: 0.015,
+        color: "white",
+      },
+    ],
+  },
+  // {
+  //   name: "Earth",
+  //   radius: 1,
+  //   distance: 20,
+  //   speed: 0.005,
+  //   material: earthMaterial,
+  //   moons: [
+  //     {
+  //       name: "Moon",
+  //       radius: 0.3,
+  //       distance: 3,
+  //       speed: 0.015,
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: "Earth",
+  //   radius: 1,
+  //   distance: 20,
+  //   speed: 0.005,
+  //   material: earthMaterial,
+  //   moons: [
+  //     {
+  //       name: "Moon",
+  //       radius: 0.3,
+  //       distance: 3,
+  //       speed: 0.015,
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: "Earth",
+  //   radius: 1,
+  //   distance: 20,
+  //   speed: 0.005,
+  //   material: earthMaterial,
+  //   moons: [
+  //     {
+  //       name: "Moon",
+  //       radius: 0.3,
+  //       distance: 3,
+  //       speed: 0.015,
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: "Earth",
+  //   radius: 1,
+  //   distance: 20,
+  //   speed: 0.005,
+  //   material: earthMaterial,
+  //   moons: [
+  //     {
+  //       name: "Moon",
+  //       radius: 0.3,
+  //       distance: 3,
+  //       speed: 0.015,
+  //     }
+  //   ]
+  // },
+];
+
+const planetMeshes = planets.map((planet) => {
+  // Create and setting the mesh
+  const planetMesh = new THREE.Mesh(sphereGeo, planet.material);
+  planetMesh.scale.setScalar(planet.radius);
+  planetMesh.position.x = planet.distance;
+
+  // Add it to the scene
+  scene.add(planetMesh);
+
+  // Loop through each moon and create the moon
+  planet.moons.forEach((moon) => {
+    // add the moon to the planet
+    const moonMesh = new THREE.Mesh(sphereGeo, moonMat);
+    moonMesh.scale.setScalar(moon.radius);
+    moonMesh.position.x = moon.distance;
+    planetMesh.add(moonMesh);
+  });
+
+  return planetMesh;
+});
+
+// Adding the meshes
+scene.add(sun);
+const bgCubeMap = cubeTextureLoader.load([
+  "px.png",
+  "nx.png",
+  "py.png",
+  "ny.png",
+  "pz.png",
+  "nz.png",
+]);
+
+scene.background = bgCubeMap;
+
+// Initializing a light
+const light = new THREE.AmbientLight("white", 0.08);
+const pointLight = new THREE.PointLight("white", 100);
+
+scene.add(light, pointLight);
 
 // Initializing a Camera
-const camera = new THREE.PerspectiveCamera(35, aspectRatio, .1, 10000);
+const camera = new THREE.PerspectiveCamera(35, aspectRatio, 0.1, 10000);
 
-camera.position.set(0, 5, 10);
+camera.position.set(0, 0, 100);
 
 // Initializing a Renderer
 const canvas = document.querySelector(".threejs");
@@ -166,22 +257,41 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(maxPixelRatio);
 
-// Applying controls
+// Applying Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 // Resize Event Handler
 window.addEventListener("resize", () => {
-  console.log("Screen resized!");
+  console.log("Screen Resized!");
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-// Creating a render loop
+// Initializing a clock
+const clock = new THREE.Clock();
+
+// Rendering the scene
 const renderloop = () => {
-  sphereGroup.children.forEach((c) => {
-    c.rotation.y += 0.01;
+  const elapsedTime = clock.getElapsedTime();
+
+  planetMeshes.forEach((planMesh, index) => {
+    planMesh.rotation.y += planets[index].speed;
+
+    planMesh.position.z =
+      Math.sin(planMesh.rotation.y) * planets[index].distance;
+    planMesh.position.x =
+      Math.cos(planMesh.rotation.y) * planets[index].distance;
+
+    planMesh.children.forEach((moonMesh, mIndex) => {
+      moonMesh.rotation.y += planets[index].moons[mIndex].speed;
+
+      moonMesh.position.x =
+        Math.sin(moonMesh.rotation.y) * planets[index].moons[mIndex].distance;
+      moonMesh.position.z =
+        Math.cos(moonMesh.rotation.y) * planets[index].moons[mIndex].distance;
+    });
   });
 
   controls.update();
