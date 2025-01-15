5376882dffb0f59828de4c5c34bf5903307f8959
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2025-01-14 23:53:43 -0300

    Tweens and timelines in GSAP; Going through basic concepts

--- Enhancing-Skills/JavaScript/GSAP/0. Installation/installation of GSAP.js
+++ Enhancing-Skills/JavaScript/GSAP/0. Installation/installation of GSAP.js
@@ -1,4 +1,4 @@
-// GSAP is a JS Library used to create animations for HTML elements by coding, It is used for complex animations involving SVG manipulation, scroll triggers and even changing basic elements properties
+// GSAP (Green Socks Animation Platform) is a JS Library used to create animations for HTML elements by coding, It is used for complex animations involving SVG manipulation, scroll triggers and even changing basic elements properties
 
 // To install the GSAP library dowload a ZIP with the main files (you can also import it through the API link) and load it into your js file
 
@@ -8,8 +8,8 @@
 // Loading with module installed:
 import gsap from "gsap";
 
-// By doing that, you're able to use all the different function the gsap module has
+// By doing that, you're able to use all the different methods the gsap module has
 
-// It's important that you register all the plugins from the module by using gsap.registerPlugin(<*pluginName>) function before using them, since  build tools can be over-aggressive and drop essential plugins for the application
+// It's important that you register all the plugins from the module by using gsap.registerPlugin(...<pluginName>) function before using them, since build tools can be over-aggressive and drop essential plugins for the application
 
 gsap.registerPlugin();
