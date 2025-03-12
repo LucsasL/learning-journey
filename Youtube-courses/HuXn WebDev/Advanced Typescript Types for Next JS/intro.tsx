4df0f3cd1e10df18c550d98f11f0a1e73f2d2e9d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2025-03-11 21:01:35 -0300

    Starting TypeScript Types for Next.js Course

---
+++ Youtube-courses/HuXn WebDev/Advanced Typescript Types for Next JS/intro.tsx
@@ -0,0 +1,28 @@
+// In this course, the fundamentals of TypeScript and Next.js won't be covered directly. Instead, the focus will be on the incorporation of types into Next.js code:
+
+// Topics to know in order to proceed
+
+// Component Props Typing
+
+// Alternative Ways
+
+// Dynamic Routes Types
+
+// Reusable Types
+
+// Types for useState()
+
+// Global Types
+
+// useRef, Forms, Events
+
+// ElementWrapper
+
+// Type Predicates
+
+// Context API Types
+
+// useReducer() Types
+
+// useEffect() Types
+
---
+++ Youtube-courses/HuXn WebDev/Advanced Typescript Types for Next JS/next-ts/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Learning Types for Next.js",
+  description: "Hello, world!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
